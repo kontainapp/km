d2214642135d23fdcc3862e7356b6fe5169c52a6
Serge Pashenkov <serge@fc.gnode.org>
2018-12-07 17:55:47 -0800

    Copyright notice

--- km_vcpu_run.c
+++ km_vcpu_run.c
@@ -1,5 +1,13 @@
 /*
- * TODO: Header
+ * Copyright © 2018 Kontain Inc. All rights reserved.
+ *
+ * Kontain Inc CONFIDENTIAL
+ *
+ * This file includes unpublished proprietary source code of Kontain Inc. The
+ * copyright notice above does not evidence any actual or intended publication of
+ * such source code. Disclosure of this source code or any related proprietary
+ * information is strictly prohibited without the express written permission of
+ * Kontain Inc.
  */
 
 #include <stdlib.h>
