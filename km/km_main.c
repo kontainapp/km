74f209c27811806cdd7c1b67e39306d2e9c45916
Mark Sterin <msterin@kontain.app>
2019-01-21 18:16:35 -0800

    Added -w flag wich forces wait for SIGUSR1 before running VCPU
    Also, see #7 (https://bitbucket.org/kontainapp/covm/issues/7)

--- km/km_main.c
+++ km/km_main.c
@@ -13,25 +13,66 @@
 #include <stdio.h>
 #include <err.h>
 #include <stdint.h>
-
+#include <getopt.h>
+#include <signal.h>
+#include <stdbool.h>
 #include <string.h>
 
 #include "km.h"
 
-int main(int argc, char const *argv[])
+
+static inline void usage()
+{
+   errx(1, "Usage: km [-w] <payload-file>\n"
+            "Options:\n\t-w - create a VM with payload and wait for SIGUSR1 before running it");
+}
+
+int main(int argc, char *const argv[])
 {
    km_vcpu_t *vcpu;
    uint64_t guest_entry, end;
+   int opt;
+   char *payload_file = NULL;
+   bool wait_for_signal = false;
 
-   if (argc != 2) {
-      err(1, "Usage: km exec_file");
+   while ((opt = getopt(argc, argv, "w")) != -1) {
+      switch (opt) {
+         case 'w':
+            wait_for_signal = true;
+            break;
+         case '?':
+         default:
+            usage();
+      }
    }
+   if (optind != argc - 1) {
+      usage();
+   }
+   payload_file = argv[optind];
+
    km_machine_init();
-   load_elf(argv[1], (void *)km_gva_to_kma(0), &guest_entry, &end);
+   load_elf(payload_file, (void *)km_gva_to_kma(0), &guest_entry, &end);
    km_mem_brk(end);
    vcpu = km_vcpu_init(guest_entry, GUEST_STACK_TOP - 1);
+
    /*
-    * Run the first vcpu
+    * If requested, wait for SIGUSR1 before running CPU. It is needed in
+    * Docker/OCI to easily support 'runc create' + 'runc delete' as a separate
+    * steps. On create, do 'km -w <payload>' , and on 'start' simply signal
+    * SIGUSR1 to the km process.
     */
+   if (wait_for_signal) {
+      sigset_t signal_set;
+      int received_signal;
+
+      sigemptyset(&signal_set);
+      sigaddset(&signal_set, SIGUSR1);
+      sigprocmask(SIG_BLOCK, &signal_set, NULL);
+      sigwait(&signal_set, &received_signal);
+  }
+
+  /*
+   * Run the first vcpu
+   */
    km_vcpu_run(vcpu);
 }
