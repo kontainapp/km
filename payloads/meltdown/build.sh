d9b35cc08427bba3fcd61903923dee15dabe08ca
Mark Sterin <msterin@kontain.app>
2019-05-07 15:45:46 -0700

    small fix to meltdown demo build

--- payloads/meltdown/build.sh
+++ payloads/meltdown/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/IAIK/meltdown
-
-(cd meltdown; patch -p1 < ../meltdown.patch; make)
+if [ ! -d meltdown ] ; then 
+  git clone https://github.com/IAIK/meltdown
+  (cd meltdown; patch -p1 < ../meltdown.patch)
+fi
+make -C meltdown
