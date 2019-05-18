72d0905c596802cee9113b8b3ff8554ee4aa9f14
Mark Sterin <msterin@kontain.app>
2019-05-18 13:43:10 -0700

    Made .km files non-executable in makefiles
    this prevents annoying typos when .km file is run directly on linux and core dumps

--- payloads/python/link-km.sh
+++ payloads/python/link-km.sh
@@ -6,3 +6,4 @@ KM_TOP=../$(git rev-parse --show-cdup)
 
 cd cpython
 ld -static -nostdlib --gc-sections -T ${KM_TOP}/km.ld -o python.km Programs/python.o libpython3.7m.a ${KM_TOP}/build/runtime/libruntime.a
+chmod a-x python.km
