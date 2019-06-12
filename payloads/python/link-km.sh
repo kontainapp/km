f8741a224941aa4455a7cd6ef707a097c32270bb
Mark Sterin <msterin@kontain.app>
2019-06-12 11:06:53 -0700

    Added zlib and queue modules to python to support Django

--- payloads/python/link-km.sh
+++ payloads/python/link-km.sh
@@ -5,5 +5,4 @@ set -x
 KM_TOP=../$(git rev-parse --show-cdup)
 
 cd cpython
-ld -static -nostdlib --gc-sections -T ${KM_TOP}/km.ld -o python.km Programs/python.o libpython3.7m.a ${KM_TOP}/build/runtime/libruntime.a
-chmod a-x python.km
+ld -static -nostdlib --gc-sections -T ${KM_TOP}/km.ld -o python.km Programs/python.o libpython3.7m.a -L/usr/lib64 -lz ${KM_TOP}/build/runtime/libruntime.a && chmod a-x python.km
