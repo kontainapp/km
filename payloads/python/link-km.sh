f3d2336fb25b6621a980c935cdae9118743c06ab
Mark Sterin <msterin@kontain.app>
2019-06-18 16:59:23 -0700

    Fixed ./payloads link to .km

--- payloads/python/link-km.sh
+++ payloads/python/link-km.sh
@@ -5,4 +5,5 @@ set -x
 KM_TOP=../$(git rev-parse --show-cdup)
 
 cd cpython
-ld -static -nostdlib --gc-sections -T ${KM_TOP}/km.ld -o python.km Programs/python.o libpython3.7m.a -L/usr/lib64 -lz ${KM_TOP}/build/runtime/libruntime.a && chmod a-x python.km
+cc -ggdb -pthread Programs/python.o libpython3.7m.a -o python.km -L/usr/lib64 -lz  -specs=${KM_TOP}/gcc-km.spec -L ${KM_TOP}/build/runtime && chmod a-x python.km
+
