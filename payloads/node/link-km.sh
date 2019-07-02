49d1c01b1523b9d575069ed19e01dc9bf29ef774
Serge Pashenkov <serge@kontain.app>
2019-07-01 18:29:08 -0700

    Link node with g++ instead of custom script. To do this needed to create an empty -lm. Add missing dl... related stubs -static -no-pie for dweb and python

---
+++ payloads/node/link-km.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+KM=../..
+NODE=node
+
+g++ -v -o $NODE/out/Release/node.km \
+  -static -no-pie -ggdb \
+  -specs=$KM/gcc-km.spec -L /home/serge/workspace/km/build/runtime \
+  -Wl,--start-group \
+    $NODE/out/Release/obj.target/node/src/node_main.o \
+    $NODE/out/Release/obj.target/node/src/node_snapshot_stub.o \
+    $NODE/out/Release/obj.target/node/gen/node_code_cache.o \
+    $NODE/out/Release/obj.target/deps/histogram/libhistogram.a \
+    $NODE/out/Release/obj.target/libnode.a \
+    $NODE/out/Release/obj.target/tools/v8_gypfiles/libv8_libplatform.a \
+    $NODE/out/Release/obj.target/tools/icu/libicui18n.a \
+    $NODE/out/Release/obj.target/deps/zlib/libzlib.a \
+    $NODE/out/Release/obj.target/deps/http_parser/libhttp_parser.a \
+    $NODE/out/Release/obj.target/deps/llhttp/libllhttp.a \
+    $NODE/out/Release/obj.target/deps/cares/libcares.a \
+    $NODE/out/Release/obj.target/deps/uv/libuv.a \
+    $NODE/out/Release/obj.target/deps/nghttp2/libnghttp2.a \
+    $NODE/out/Release/obj.target/deps/brotli/libbrotli.a \
+    $NODE/out/Release/obj.target/deps/openssl/libopenssl.a \
+    $NODE/out/Release/obj.target/tools/v8_gypfiles/libv8_base.a \
+    $NODE/out/Release/obj.target/tools/v8_gypfiles/libv8_libbase.a \
+    $NODE/out/Release/obj.target/tools/v8_gypfiles/libv8_libsampler.a \
+    $NODE/out/Release/obj.target/tools/icu/libicuucx.a \
+    $NODE/out/Release/obj.target/tools/icu/libicudata.a \
+    $NODE/out/Release/obj.target/tools/icu/libicustubdata.a \
+    $NODE/out/Release/obj.target/tools/v8_gypfiles/libv8_snapshot.a \
+    $NODE/out/Release/obj.target/tools/v8_gypfiles/libgenerate_snapshot.a \
+  -Wl,--end-group -zundefs
--- payloads/python/link-km.sh
+++ payloads/python/link-km.sh
@@ -5,5 +5,5 @@ set -x
 KM_TOP=../$(git rev-parse --show-cdup)
 
 cd cpython
-cc -ggdb -pthread Programs/python.o libpython3.7m.a -o python.km -L/usr/lib64 -lz  -specs=${KM_TOP}/gcc-km.spec -L ${KM_TOP}/build/runtime && chmod a-x python.km
+cc -static -no-pie -ggdb -pthread Programs/python.o libpython3.7m.a -o python.km -L/usr/lib64 -lz  -specs=${KM_TOP}/gcc-km.spec -L ${KM_TOP}/build/runtime && chmod a-x python.km
 
