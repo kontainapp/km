4d2bb50596c8c601c7f0d05752536fb5a34956b4
Serge <52137221+gnode1@users.noreply.github.com>
2019-07-22 21:35:58 -0700

    Revert "set tid in pthread in a manner consistent with assumptions in mutex code and other places"
    This reverts commit a3e257c0efbd3e3bf503755f6082edf343cab135.

--- km/km_vcpu_run.c
+++ km/km_vcpu_run.c
@@ -597,7 +597,7 @@ static int km_vcpu_one_kvm_run(km_vcpu_t* vcpu)
 void* km_vcpu_run(km_vcpu_t* vcpu)
 {
    int status, hc;
-   km_vcpu_set_tid(vcpu);
+   vcpu->tid = gettid();
    vcpu->is_paused = 1;
 
    while (1) {
@@ -695,7 +695,8 @@ void* km_vcpu_run(km_vcpu_t* vcpu)
 void* km_vcpu_run_main(void* unused)
 {
    km_vcpu_t* vcpu = km_main_vcpu();
-   km_vcpu_set_tid(vcpu);
+
+   vcpu->tid = gettid();
 
    km_install_sighandler(KM_SIGVCPUSTOP, km_vcpu_pause_sighandler);
    km_install_sighandler(SIGPIPE, km_forward_fd_signal);
