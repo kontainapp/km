9d29b89c6608d8bb7a7dca61cf172ab2fcaaaf01
Serge Pashenkov <serge@kontain.app>
2019-08-16 12:33:42 -0700

    cleanup in kontain-gcc, use it for node and python

--- payloads/python/link-km.sh
+++ payloads/python/link-km.sh
@@ -3,7 +3,8 @@
 set -x
 
 KM_TOP=../$(git rev-parse --show-cdup)
+PATH=${KM_TOP}/tools:$PATH
 
 cd cpython
-cc -static -no-pie -ggdb -pthread Programs/python.o libpython3*.a -o python.km -L/usr/lib64 -lz  -specs=${KM_TOP}/gcc-km.spec -L ${KM_TOP}/build/runtime && chmod a-x python.km
+kontain-gcc -ggdb Programs/python.o libpython3*.a -lz -o python.km && chmod a-x python.km
 
