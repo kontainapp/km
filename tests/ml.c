994ce2ae170ca1c892bdb3f55b63116a5843eb92
Mark Sterin <msterin@kontain.app>
2019-08-22 14:02:04 -0700

    Review fixes: rearranged nolock approach and added copyrigt msg

--- tests/ml.c
+++ tests/ml.c
@@ -1,4 +1,16 @@
-// check mprotect /map glueing things together. borrowed from man mprotect
+/*
+ * Copyright © 2019 Kontain Inc. All rights reserved.
+ *
+ * Kontain Inc CONFIDENTIAL
+ *
+ * This file includes unpublished proprietary source code of Kontain Inc. The
+ * copyright notice above does not evidence any actual or intended publication
+ * of such source code. Disclosure of this source code or any related
+ * proprietary information is strictly prohibited without the express written
+ * permission of Kontain Inc.
+ *
+ * Manual helper to experiment with mprotect /map glueing in Linux . Based on 'man mprotect'
+ */
 
 #define _GNU_SOURCE /* See feature_test_macros(7) */
 
