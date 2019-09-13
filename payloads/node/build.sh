9501379f1e493be085efe51d017eefa1973a964b
Serge Pashenkov <serge@kontain.app>
2019-09-13 09:25:59 -0700

    build and use our own libstdc++ instead of system standard, due to conflict with musl locale staged fedora dockerfile for build image with libstdc++
    UID:GUD in Dockerfile and make withdocker

--- payloads/node/build.sh
+++ payloads/node/build.sh
@@ -5,6 +5,9 @@ set -x
 if [ $# -ne 0 ] ; then BUILD=$1 ; else BUILD=Release ; fi
 if [ $BUILD == 'Debug' ] ; then CONFIG_DEBUG=--debug ; fi
 
+NODE=node/out/$BUILD/node
+KM=`realpath ../../build/km/km`
+
 if [ ! -d node ]; then
     git clone https://github.com/nodejs/node.git -b v12.4.0
     pushd node
@@ -14,11 +17,16 @@ else
     pushd node
 fi
 
-make -j8
+make -j16
 popd
 
 ./link-km.sh $BUILD
+mv $NODE ${NODE}.static
+
+echo "#!$KM --copyenv" > ${NODE}.sh
+chmod +x ${NODE}.sh
+ln -s node.sh ${NODE}
 
 echo ""
-echo "now you can run ``../../build/km/km ./node/out/${BUILD}/node.km''"
+echo "now you can run ``$KM ${NODE}.km''"
 
