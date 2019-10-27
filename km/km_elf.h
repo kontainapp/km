3f01ad56d7597aa6031ca2b2eb48b3c157eec065
Serge Pashenkov <serge@kontain.app>
2019-10-26 18:49:15 -0700

    checkpoint -- single thread seems to be OK

--- km/km_elf.h
+++ km/km_elf.h
@@ -19,11 +19,7 @@
 #include <gelf.h>
 #include <stdint.h>
 
-#define KM_LIBC_SYM_NAME "__libc"
-#define KM_THR_START_SYM_NAME "__start_thread__"
 #define KM_INT_HNDL_SYM_NAME "__km_handle_interrupt"
-#define KM_TSD_SIZE_SYM_NAME "__pthread_tsd_size"
-#define KM_PCREATE_SYM_NAME "pthread_create"
 #define KM_SIG_RTRN_SYM_NAME "__km_sigreturn"
 /*
  * Description of the guest payload. Note these structures come from guest ELF and represent values
@@ -32,10 +28,7 @@
 typedef struct km_payload {
    Elf64_Ehdr km_ehdr;           // elf file header
    Elf64_Phdr* km_phdr;          // elf program headers
-   Elf64_Addr km_libc;           // libc in payload program
-   Elf64_Addr km_start_thread;   // thread start wrapper
    Elf64_Addr km_handlers;       // interrupt/exception handler
-   Elf64_Addr km_tsd_size;       // __pthread_tsd_size in the payload
    Elf64_Addr km_sigreturn;      // signal trampoline function
 } km_payload_t;
 
