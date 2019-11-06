2926f64afbdff5738f818d8211f23abbd581c10d
Mark Sterin <msterin@kontain.app>
2019-11-06 01:18:55 -0800

    using jinja2 templates for .c generator

--- payloads/python/extensions/dltables.py
+++ payloads/python/extensions/dltables.py
@@ -1,12 +1,55 @@
 #!/bin/env python3
 #
 # prep dlopen/dlsym tables for a module from bear.out
-# set -x
+#
+
 import os
 import subprocess
 import sys
 import sysconfig
 import hashlib
+import jinja2  # TODO - make sure it's installed (run pip3 install --user)
+
+# setup.py puts binary builds here. PYTHON SPECIFIC
+BUILD_LIB_PREFIX = "build/lib.{}-{}/".format(sysconfig.get_platform(),
+                                             sysconfig.get_python_version())
+
+# jinja2 template for generated .c file.
+symfile_template = """
+/*
+ * GENERATED FILE. DO NOT EDIT.
+ *
+ * Copyright © 2019 Kontain Inc. All rights reserved.
+ *
+ * Kontain Inc CONFIDENTIAL
+ *
+ * This file includes unpublished proprietary source code of Kontain Inc. The
+ * copyright notice above does not evidence any actual or intended publication
+ * of such source code. Disclosure of this source code or any related
+ * proprietary information is strictly prohibited without the express written
+ * permission of Kontain Inc.
+ *
+ * Registration and dynsym tables for {{ so }}
+ */
+
+# include "dlinfo_km.h"
+
+extern void * {{ symbols[0].munged }} {% for sym in symbols[1:] %}, *{{ sym.munged }} {% endfor %};
+
+static km_dl_symbols_t _km_symbols[] = { {% for sym in symbols %}
+   { .name = "{{ sym.name }}", .sym = .&{{ sym.munged }} }, {% endfor %}
+
+   { .name = NULL, .sym = NULL }
+};
+
+static km_dl_info_t km_dl_info = {
+   .name = "{{ mod }}", .symtable = _km_symbols
+};
+
+__attribute__((constructor))  static void _km_dlinit(void) {
+   km_dl_register(&km_dl_info);
+}
+"""
 
 
 def python_mod_convert(so_name, so_suffix):
@@ -55,6 +98,7 @@ def process_file(file_name, so_suffix=".cpython-37m-x86_64-linux-gnu.so", conver
 
         nm = subprocess.run(["nm", "-s", "--defined-only", "-Dg", os.path.join(location, so_name)],
                             capture_output=True, encoding="utf-8")
+        # nm.stdout is a list of nm output lines, each has "address TYPE symname". Extract symnames array:
         names = [i.split()[2] for i in nm.stdout.splitlines()]
 
         # python-specific module
@@ -62,56 +106,15 @@ def process_file(file_name, so_suffix=".cpython-37m-x86_64-linux-gnu.so", conver
         with open(os.path.join(location, so_name.replace(so_suffix, ".sym.txt")), 'w') as f:
             f.writelines(["{} {}\n".format(i, mod_sym(i, mod)) for i in names])
 
-        # ===== each generated file starts with this:
-        dlsym_prologue = """
-/*
- * GENERATED FILE. DO NOT EDIT. See dltables.py
- *
- * registration and dynsym tables for {so}
- */
-
-# include "dlinfo_km.h"
-
-extern void *{names_def};
-
-static km_dl_symbols_t _km_symbols[] = {{
-"""
-
-        # ==== each generated file ends with this:
-        dlsym_epilogue = """
-
-      {{  .name = NULL, .sym = NULL }}
-}};
-
-
-static km_dl_info_t km_dl_info = {{
-   .name = "{mod}", .symtable = _km_symbols
-}};
-
-__attribute__((constructor))  static void _km_dlinit(void) {{
-   km_dl_register(&km_dl_info);
-}}
-
-"""
-
-        with open(os.path.join(location, mod + "_dlsym.c"), 'x') as f:
-            f.write(dlsym_prologue.format(so=so_name, names_def=", *".join([mod_sym(i, mod) for i in names])))
-            for n in names:
-                f.write("      {{ .name = \"{sym}\", .sym = &{mod_sym} }},\n".format(
-                    sym=n, mod_sym=mod_sym(n, mod)))
-            f.write(dlsym_epilogue.format(mod=mod))
-
-    # mod_name =$(basename $so | sed "s/$so_suffix//")
-    # out =${mod_name}_table.c
-    # nm - s - -defined-only - Dg $so | awk "$(m4 -DMODULE=$mod_name $exedir/awk_script)" > $out
-    # done
+        with open(os.path.join(location, mod + "_dlsym.c"), 'w') as f:
+            symbols = [{"name": n, "munged": mod_sym(n, mod)} for n in names]
+            f.write(jinja2.Template(symfile_template).render(so=so_name[len(BUILD_LIB_PREFIX):],
+                                                             mod=mod,
+                                                             symbols=symbols))
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("""
-Usage: {} file_name
-   file is .so name
-""".format(sys.argv[0]))
+        print("Usage: {} file_name\n   file contains output with .so build commands".format(sys.argv[0]))
     else:
         process_file(sys.argv[1])
