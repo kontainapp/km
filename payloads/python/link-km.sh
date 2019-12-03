8dc9c85ed8959720332cb7dbe278a93bcfe6a4ea
Serge Pashenkov <serge@kontain.app>
2019-12-03 10:27:42 -0800

    fix python link-km.sh rot

--- payloads/python/link-km.sh
+++ payloads/python/link-km.sh
@@ -2,11 +2,11 @@
 
 set -x
 
-KM_TOP=../$(git rev-parse --show-cdup)
+KM_TOP=$(git rev-parse --show-cdup)
 PATH=${KM_TOP}/tools:$PATH
 
-BUILD=${1:cpython}
-OUT=${2:cpython}
+BUILD=${1:-cpython}
+OUT=${2:-cpython}
 
 kontain-gcc -ggdb ${BUILD}/Programs/python.o ${BUILD}/libpython3*.a -lz -lssl -lcrypto -o ${OUT}/python.km && chmod a-x ${OUT}/python.km
 
