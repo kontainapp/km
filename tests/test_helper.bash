05d844084a3765fdeeb82622781640d968966e85
Mark Sterin <msterin@kontain.app>
2019-12-31 21:07:18 -0800

    Avoid hw break test on AMD, and compile all with -ggdb3  (#372)
    * Avoid hw break test on AMD, and compile all with -ggdb3
    
    Fixes #368 (GDB tests fail on AMD CPUs) by using 'break' in the test on AMD, instead of 'hbreak', and
    modifying stub to report sw break if dr6/7 (such as amd regs behave in our code now)
    
    Also 
    * adds -ggdb3 instead of -ggdb , to save macros info in gdb tables
    * minor change to some infox formats to avoid line split in code
    
    tested with make tests on AMD Ryzen machine (at my home) and Intel Xeon box (at the office)

--- tests/test_helper.bash
+++ tests/test_helper.bash
@@ -54,12 +54,17 @@ function km_with_timeout () {
    s=$?; if [ $s -eq 124 ] ; then echo "\nTimed out in $timeout" ; fi ; return $s
 }
 
+if grep -iq 'vendor_id.*:.*intel' /proc/cpuinfo
+then
+   check_hwbreak=yes
+fi
 # this is how we invoke gdb - with timeout
 function gdb_with_timeout () {
    timeout --foreground $timeout \
       gdb "$@"
    s=$?; if [ $s -eq 124 ] ; then echo "\nTimed out in $timeout" ; fi ; return $s
 }
+
 
 # this is needed for running in Docker - bats uses 'tput' so it needs the TERM
 TERM=xterm
