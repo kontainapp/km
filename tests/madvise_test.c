45023a88b09d9af6e8bba2d6d0fa7fb0bdd3469d
Isaac Pashenkov <isaac@kontain-laptop.hsd1.ca.comcast.net>
2020-01-06 10:47:23 -0800

    Addressed Reviews
    WIP madvise fix

--- tests/madvise_test.c
+++ tests/madvise_test.c
@@ -159,22 +159,17 @@ TEST simple_test()
        {__LINE__, "1. Large Empty mmap", TYPE_MMAP, 0, 64 * MIB, PROT_WRITE, flags, OK},
        {__LINE__, "2a. OK to WRITE", TYPE_WRITE, 5 * MIB, 20 * MIB, '2', 0, OK},
        {__LINE__, "3. madvise MADV_DONTNEED should fail", TYPE_MADVISE, 100 * MIB, 10 * MIB, 0, flags, ENOMEM, MADV_DONTNEED},
-       {__LINE__,
-        "3a. madvise MADV_FREE, EINVAL fail",
-        TYPE_MADVISE,
-        8 * MIB,
-        10 * MIB,
-        0,
-        flags,
-        EINVAL,
-        MADV_FREE},   // Only MADV_DONTNEED
+       {__LINE__, "3a. madvise MADV_FREE, EINVAL fail", TYPE_MADVISE, 8 * MIB, 10 * MIB, 0, flags, EINVAL, MADV_FREE},
+       {__LINE__, "3b. OK to READ, should read in not 0", TYPE_READ, 9 * MIB, 1 * MIB, '2', 0, OK, 0},
+       {__LINE__, "4. OK to READ", TYPE_READ, 9 * MIB, 1 * MIB, 0, 0, OK, 0},   // should
+                                                                                // succeed but
+                                                                                // does not on
+                                                                                // KM
+       {__LINE__, "4a. OK to READ", TYPE_READ, 9 * MIB, 1 * MIB, 0, 0, OK, 0},
+       {__LINE__, "5. OK to WRITE", TYPE_WRITE, 9 * MIB, 1 * MIB, '2', 0, OK},
        {__LINE__, "3b. OK to READ, should read in not 0", TYPE_READ, 9 * MIB, 1 * MIB, '2', 0, OK, 0},
        {__LINE__, "3c. madvise MADV_DONTNEED", TYPE_MADVISE, 8 * MIB, 10 * MIB, 0, flags, OK, MADV_DONTNEED},
-       {__LINE__, "4. OK to READ", TYPE_READ, 9 * MIB, 1 * MIB, 0, 0, SIGSEGV, 0},   // should succeed but
-                                                                                     // does not on KM
    };
-   printf("Running %s\n", __FUNCTION__);
-   CHECK_CALL(madvice_execute(madvice_tests));
    PASS();
 }
 GREATEST_MAIN_DEFS();
