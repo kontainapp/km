2f6a3e98daff5eed64be406825fed1bebae80cef
Isaac Pashenkov <isaac@kontain-laptop.hsd1.ca.comcast.net>
2020-01-06 17:14:01 -0800

    Test Fix

--- tests/madvise_test.c
+++ tests/madvise_test.c
@@ -161,12 +161,12 @@ TEST simple_test()
        {__LINE__, "3. madvise MADV_DONTNEED should fail", TYPE_MADVISE, 100 * MIB, 10 * MIB, 0, flags, ENOMEM, MADV_DONTNEED},
        {__LINE__, "3a. madvise MADV_FREE, EINVAL fail", TYPE_MADVISE, 8 * MIB, 10 * MIB, 0, flags, EINVAL, MADV_FREE},
        {__LINE__, "3b. OK to READ, should read in not 0", TYPE_READ, 9 * MIB, 1 * MIB, '2', 0, OK, 0},
-       {__LINE__, "4. OK to READ", TYPE_READ, 9 * MIB, 1 * MIB, 0, 0, OK, 0},
+       {__LINE__, "3c. madvise MADV_DONTNEED", TYPE_MADVISE, 8 * MIB, 10 * MIB, 0, flags, OK, MADV_DONTNEED},
        {__LINE__, "4a. OK to READ", TYPE_READ, 9 * MIB, 1 * MIB, 0, 0, OK, 0},
        {__LINE__, "5. OK to WRITE", TYPE_WRITE, 9 * MIB, 1 * MIB, '2', 0, OK},
-       {__LINE__, "3b. OK to READ, should read in not 0", TYPE_READ, 9 * MIB, 1 * MIB, '2', 0, OK, 0},
-       {__LINE__, "3c. madvise MADV_DONTNEED", TYPE_MADVISE, 8 * MIB, 10 * MIB, 0, flags, OK, MADV_DONTNEED},
    };
+   printf("Running %s\n", __FUNCTION__);
+   CHECK_CALL(madvice_execute(madvice_tests));
    PASS();
 }
 GREATEST_MAIN_DEFS();
