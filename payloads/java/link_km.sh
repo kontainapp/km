851af854a540c51326e7a3f2ff7bbe2b5234e1cc
John Muth <muth@kontain.app>
2020-01-07 17:23:06 -0800

    Build fix

--- payloads/java/link_km.sh
+++ payloads/java/link_km.sh
@@ -4,9 +4,10 @@
 #
 set -x
 
+JDK_DIR=jdk-11+28
 KM_TOP=$(git rev-parse --show-cdup) 
-${KM_TOP}/tools/kontain-gcc -rdynamic -Wl,--rpath=/opt/kontain/lib64:/lib64:jdk/build/linux-x86_64-server-release/jdk/lib:build/linux-x86_64-server-release/jdk/lib/server \
+${KM_TOP}/tools/kontain-gcc -rdynamic -Wl,--rpath=/opt/kontain/lib64:/lib64:${JDK_DIR}/build/linux-x86_64-server-release/jdk/lib:build/linux-x86_64-server-release/jdk/lib/server \
     -Wl,--hash-style=both -Wl,-z,defs -Wl,-z,noexecstack -Wl,-O1 -m64 -Wl,--allow-shlib-undefined -Wl,--exclude-libs,ALL \
-    -Wl,-rpath,\$ORIGIN -Wl,-rpath,\$ORIGIN/../lib -Ljdk/build/linux-x86_64-server-release/support/modules_libs/java.base \
-    -o jdk/build/linux-x86_64-server-release/support/native/java.base/java_objs/java.kmd \
-    jdk/build/linux-x86_64-server-release/support/native/java.base/java/main.o -ljli -lpthread -ldl
+    -Wl,-rpath,\$ORIGIN -Wl,-rpath,\$ORIGIN/../lib -L${JDK_DIR}/build/linux-x86_64-server-release/support/modules_libs/java.base \
+    -o ${JDK_DIR}/build/linux-x86_64-server-release/support/native/java.base/java_objs/java.kmd \
+    ${JDK_DIR}/build/linux-x86_64-server-release/support/native/java.base/java/main.o -ljli -lpthread -ldl
