1f863b342b6381ae6fbf72dee024e66ca716194a
Mark Sterin <msterin@kontain.app>
2020-01-31 12:46:45 -0800

    Fixes for Java and kafka running under Kontain
    * Added kafka-net to sanity check run
    * remove orig lib from kontainer
    * Moved payloads to java 11
    * Adjusted docker/demo for java 11 and new libc.so location

--- payloads/java/link_km.sh
+++ payloads/java/link_km.sh
@@ -4,10 +4,12 @@
 #
 set -x
 
-JDK_DIR=jdk-11+28
+JDK_DIR=jdk-11+28/build/linux-x86_64-normal-server-release/
 KM_TOP=$(git rev-parse --show-cdup) 
-${KM_TOP}/tools/kontain-gcc -rdynamic -Wl,--rpath=/opt/kontain/lib64:/lib64:${JDK_DIR}/build/linux-x86_64-server-release/jdk/lib:build/linux-x86_64-server-release/jdk/lib/server \
+${KM_TOP}/tools/kontain-gcc -rdynamic -Wl,--rpath=/opt/kontain/lib64:/lib64:${JDK_DIR}jdk/lib:${JDK_DIR}jdk/lib/server \
     -Wl,--hash-style=both -Wl,-z,defs -Wl,-z,noexecstack -Wl,-O1 -m64 -Wl,--allow-shlib-undefined -Wl,--exclude-libs,ALL \
-    -Wl,-rpath,\$ORIGIN -Wl,-rpath,\$ORIGIN/../lib -L${JDK_DIR}/build/linux-x86_64-server-release/support/modules_libs/java.base \
-    -o ${JDK_DIR}/build/linux-x86_64-server-release/images/jdk/bin/java.kmd \
-    ${JDK_DIR}/build/linux-x86_64-server-release/support/native/java.base/java/main.o -ljli -lpthread -ldl
+    -Wl,-rpath,\$ORIGIN -Wl,-rpath,\$ORIGIN/../lib \
+    -L${JDK_DIR}support/modules_libs/java.base \
+    -L${JDK_DIR}support/modules_libs/java.base/jli \
+    -o ${JDK_DIR}images/jdk/bin/java.kmd \
+    ${JDK_DIR}support/native/java.base/java/main.o -ljli -lpthread -ldl
