0fc85573523f1d9236cb82e7183033716cca7f18
Serge Pashenkov <serge@kontain.app>
2020-03-12 12:19:02 -0700

    fix compile warning in __unmapself()

--- runtime/unmapself_km.c
+++ runtime/unmapself_km.c
@@ -6,7 +6,9 @@ _Noreturn void __unmapself(void* base, size_t size)
 {
    km_hc_args_t args = {0};
 
-   args.arg1 = base;
+   args.arg1 = (uint64_t)base;
    args.arg2 = size;
-   km_hcall(HC_unmapself, &args);
+   while (1) {
+      km_hcall(HC_unmapself, &args);
+   }
 }
