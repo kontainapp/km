99b4e9ed77123feb0cd192418ed638d7a70111e8
Mark Sterin <msterin@kontain.app>
2020-03-16 17:03:14 -0700

    Added buildenv-image and runenv-image support for payloads/java (#495)
    * Added buildenv-image and runenv-image support for payloads/java
    
    * reworked targets and behavior of Makefile
    * can build java locally or from buildenv image
    * runtime image has Java modules and .so and is ~170MB
    * 'make test' is provided
    
    Also added validation check for all payloads
    Tested manually

--- payloads/java/link_km.sh
+++ payloads/java/link_km.sh
@@ -4,12 +4,20 @@
 #
 set -x
 
-JDK_DIR=jdk-11.0.6+10/build/linux-x86_64-normal-server-release/
-KM_TOP=$(git rev-parse --show-cdup) 
-${KM_TOP}/tools/kontain-gcc -rdynamic -Wl,--rpath=/opt/kontain/lib64:/lib64:${JDK_DIR}jdk/lib:${JDK_DIR}jdk/lib/server \
-    -Wl,--hash-style=both -Wl,-z,defs -Wl,-z,noexecstack -Wl,-O1 -m64 -Wl,--allow-shlib-undefined -Wl,--exclude-libs,ALL \
-    -Wl,-rpath,\$ORIGIN -Wl,-rpath,\$ORIGIN/../lib \
-    -L${JDK_DIR}support/modules_libs/java.base \
-    -L${JDK_DIR}support/modules_libs/java.base/jli \
-    -o ${JDK_DIR}images/jdk/bin/java.kmd \
-    ${JDK_DIR}support/native/java.base/java/main.o -ljli -lpthread -ldl
+KM_TOP=$(git rev-parse --show-toplevel)
+if [[ -z "$1" ]] ; then
+   echo Usage: link_km.sh jdk_dir
+   exit 1
+fi
+
+JDK_DIR=$1
+OUT_DIR=${JDK_DIR}/images/jdk/bin
+
+mkdir -p ${OUT_DIR}
+${KM_TOP}/tools/kontain-gcc -rdynamic \
+    -Wl,--hash-style=both -Wl,-z,defs -Wl,-z,noexecstack \
+    -Wl,-O1 -m64 -Wl,--allow-shlib-undefined -Wl,--exclude-libs,ALL \
+    ${JDK_DIR}/support/native/java.base/java/main.o \
+    -L${JDK_DIR}/images/jdk/lib/jli -ljli \
+    -o ${OUT_DIR}/java.kmd
+
