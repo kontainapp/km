10017699d07d69c6086d9afc9a0ba622167e4f3f
yihuaf <fang.yihua.eric@gmail.com>
2020-06-11 15:10:29 -0700

    spring boot demo automation (#667)

---
+++ demo/spring-boot/test/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright © 2020 Kontain Inc. All rights reserved.
+#
+# Kontain Inc CONFIDENTIAL
+#
+#  This file includes unpublished proprietary source code of Kontain Inc. The
+#  copyright notice above does not evidence any actual or intended publication of
+#  such source code. Disclosure of this source code or any related proprietary
+#  information is strictly prohibited without the express written permission of
+#  Kontain Inc.
+#
+
+attempt_counter=0
+max_attempts=25
+
+until $(curl --output /dev/null --silent --fail http://localhost:8080/greeting); do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+      echo "Max attempts reached"
+      exit 1
+    fi
+
+    printf '.'
+    attempt_counter=$(($attempt_counter+1))
+    sleep 1
+done
+
+echo "Test Passed"
