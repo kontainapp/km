3cade9a066d06fb6907785033f1fc74349652257
yihuaf <efang@kontain.app>
2020-07-02 02:35:21 -0400

    remake the build system

---
+++ demo/beta/python/components.mk
@@ -0,0 +1,17 @@
+${COMPONENT}_PATH := $(realpath ${COMPONENT})
+${COMPONENT}_IMAGE_NAME := docker.io/kontainapp/${COMPONENT}-demo-${IMAGE_VERSION}
+${COMPONENT}_DOCKER_IMAGE_TAG := ${${COMPONENT}_IMAGE_NAME}:docker
+${COMPONENT}_KONTAIN_IMAGE_TAG := ${${COMPONENT}_IMAGE_NAME}:kontain
+
+${COMPONENT}/docker: COMPONENT := ${COMPONENT}
+${COMPONENT}/docker: base
+	${DOCKER_BUILD} -t ${${COMPONENT}_DOCKER_IMAGE_TAG} -f ${${COMPONENT}_PATH}/docker.dockerfile ${${COMPONENT}_PATH} 
+
+${COMPONENT}/kontain: COMPONENT := ${COMPONENT}
+${COMPONENT}/kontain: base
+	${DOCKER_BUILD} -t ${${COMPONENT}_KONTAIN_IMAGE_TAG} -f ${${COMPONENT}_PATH}/kontain.dockerfile ${${COMPONENT}_PATH} 
+
+${COMPONENT}/clean: COMPONENT := ${COMPONENT}
+${COMPONENT}/clean:
+	$(call clean_container_image,${${COMPONENT}_DOCKER_IMAGE_TAG})
+	$(call clean_container_image,${${COMPONENT}_KONTAIN_IMAGE_TAG})
