f17d2617dc5e603132c73acd33aad1729382bf99
Paul Popelka <paulp@kontain.app>
2020-07-27 11:06:02 -0700

    Address review comment.

--- tests/syscallexer_test.c
+++ tests/syscallexer_test.c
@@ -12,7 +12,7 @@
 
 /*
  * This is a small program that runs 3 syscalls/hypercalls that hopefully do very little in
- * the kernel.  The intent is to measure in a squishy what how adding arguments to syscalls
+ * the kernel.  The intent is to measure in a squishy way how adding arguments to syscalls
  * increases the overhead.  The syscalls chosen have no arguments, 1 argument, and 6 arguments.
  * Each syscall is run 5 million times and the length of time is measured and printed.
  * In addition 1 or more threads can be doing these syscall runs concurrently to see how
@@ -21,6 +21,11 @@
  * a pool of km_hc_arg_t's and allocating one for each syscall and then freeing it when the
  * syscall returns.  This method would have affects on scaling.  This was being considered for
  * running go programs under km.
+ *
+ * Running this program by itself just gives you a set of data points for a particular way
+ * of passing hypercall arguments to km.  This program should be run for each hypercall arg
+ * passing method to get timing data for each and then comparisons can be made to decide
+ * which method is better.
  *
  * One more thing to consider when trying to measure syscall overhead is you need to actually
  * disassemble the syscall c library function to see what is really going on.  Some of the
