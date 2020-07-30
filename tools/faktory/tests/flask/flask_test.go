f289ce4e08827823a9c8a46732692ce102b6eb24
yihuaf <efang@kontain.app>
2020-07-30 18:46:13 -0400

    fixed tests

--- tools/faktory/tests/flask/flask_test.go
+++ tools/faktory/tests/flask/flask_test.go
@@ -1,16 +1,11 @@
 package test
 
 import (
-	"context"
 	"net/http"
 	"os/exec"
 	"path"
 	"testing"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/client"
-	"github.com/docker/go-connections/nat"
 	"github.com/hashicorp/go-retryablehttp"
 	"github.com/pkg/errors"
 )
@@ -22,7 +17,7 @@ var faktoryBin string = path.Join("./", "../../bin/faktory")
 
 func runTest() error {
 	retryClient := retryablehttp.NewClient()
-	retryClient.RetryMax = 10
+	retryClient.RetryMax = 3
 	resp, err := retryClient.Get("http://127.0.0.1:8080")
 	if err != nil {
 		return errors.Wrap(err, "Failed to make the http call")
@@ -39,7 +34,8 @@ func runTest() error {
 // original base as the conversion base. This will make the rootfs of the
 // converted image the same as the original.
 func testDocker(t *testing.T) error {
-	const BASE string = "python:3.8-alpine"
+	const BASE string = "python:3.7-alpine"
+	const TESTCONTAINER string = "faktory_test_docker"
 
 	// Build the from image
 	if err := exec.Command("docker", "build", "-t", FROM, "assets/").Run(); err != nil {
@@ -50,44 +46,22 @@ func testDocker(t *testing.T) error {
 		return errors.Wrap(err, "Failed to convert")
 	}
 
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		return errors.Wrap(err, "Failed to create a docker client")
-	}
-
-	ret, err := cli.ContainerCreate(ctx,
-		&container.Config{
-			Image: TO,
-			ExposedPorts: nat.PortSet{
-				"8080/tcp": {},
-			},
-		},
-		&container.HostConfig{
-			AutoRemove: true,
-			PortBindings: nat.PortMap{
-				"8080/tcp": []nat.PortBinding{
-					{
-						HostPort: "8080",
-					},
-				},
-			},
-		},
-		nil,
-		"",
-	)
-	if err != nil {
+	if err := exec.Command("docker",
+		"run",
+		"-d",
+		"--rm",
+		"-p", "8080:8080",
+		"--name", TESTCONTAINER,
+		TO).Run(); err != nil {
 		return errors.Wrap(err, "Failed to create container")
 	}
 
-	if err := cli.ContainerStart(ctx, ret.ID, types.ContainerStartOptions{}); err != nil {
-		return errors.Wrap(err, "Failed to start container")
-	}
-
-	defer cli.ContainerStop(ctx, ret.ID, nil)
-
 	if err := runTest(); err != nil {
 		return errors.Wrap(err, "Failed to test the converted image")
+	}
+
+	if err := exec.Command("docker", "stop", TESTCONTAINER).Run(); err != nil {
+		return errors.Wrap(err, "Failed to stop testing container")
 	}
 
 	exec.Command("docker", "rmi", TO).Run()
@@ -98,6 +72,7 @@ func testDocker(t *testing.T) error {
 
 func testKontain(t *testing.T) error {
 	const BASE string = "kontain/runenv-python:latest"
+	const TESTCONTAINER string = "faktory_test_kontain"
 
 	// Build the from image
 	if err := exec.Command("docker", "build", "-t", FROM, "assets/").Run(); err != nil {
@@ -108,65 +83,41 @@ func testKontain(t *testing.T) error {
 		return errors.Wrap(err, "Failed to convert")
 	}
 
-	// ctx := context.Background()
-	// cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	// if err != nil {
-	// 	return errors.Wrap(err, "Failed to create a docker client")
-	// }
+	if err := exec.Command("docker",
+		"run",
+		"-d",
+		"--rm",
+		"--device=/dev/kvm",
+		"-v", "/opt/kontain/bin/km:/opt/kontain/bin/km:z",
+		"-p", "8080:8080",
+		"--name", TESTCONTAINER,
+		TO).Run(); err != nil {
+		return errors.Wrap(err, "Failed to create container")
+	}
 
-	// ret, err := cli.ContainerCreate(ctx,
-	// 	&container.Config{
-	// 		Image:      TO,
-	// 		WorkingDir: "/app",
-	// 		Cmd:        []string{"gunicorn", "--bind", "0.0.0.0:8080", "app:app"},
-	// 		ExposedPorts: nat.PortSet{
-	// 			"8080/tcp": {},
-	// 		},
-	// 	},
-	// 	&container.HostConfig{
-	// 		AutoRemove: true,
-	// 		PortBindings: nat.PortMap{
-	// 			"8080/tcp": []nat.PortBinding{
-	// 				{
-	// 					HostPort: "8080",
-	// 				},
-	// 			},
-	// 		},
-	// 	},
-	// 	nil,
-	// 	"",
-	// )
-	// if err != nil {
-	// 	return errors.Wrap(err, "Failed to create container")
-	// }
+	if err := runTest(); err != nil {
+		return errors.Wrap(err, "Failed to test the converted image")
+	}
 
-	// if err := cli.ContainerStart(ctx, ret.ID, types.ContainerStartOptions{}); err != nil {
-	// 	return errors.Wrap(err, "Failed to start container")
-	// }
+	if err := exec.Command("docker", "stop", TESTCONTAINER).Run(); err != nil {
+		return errors.Wrap(err, "Failed to stop testing container")
+	}
 
-	// defer cli.ContainerStop(ctx, ret.ID, nil)
-
-	// if err := runTest(); err != nil {
-	// 	return errors.Wrap(err, "Failed to test the converted image")
-	// }
-
-	// exec.Command("docker", "rmi", TO).Run()
-	// exec.Command("docker", "rmi", FROM).Run()
+	exec.Command("docker", "rmi", TO).Run()
+	exec.Command("docker", "rmi", FROM).Run()
 
 	return nil
 }
 
 func TestFlask(t *testing.T) {
-
-	// t.Run("Test with docker images", func(t *testing.T) {
-	// 	if err := testDocker(t); err != nil {
-	// 		t.Fatalf("Failed test: %v", err)
-	// 	}
-	// })
+	t.Run("Test with docker images", func(t *testing.T) {
+		if err := testDocker(t); err != nil {
+			t.Fatalf("Failed test: %v", err)
+		}
+	})
 	t.Run("Test with kontain images", func(t *testing.T) {
 		if err := testKontain(t); err != nil {
 			t.Fatalf("Failed test: %v", err)
 		}
 	})
-
 }
