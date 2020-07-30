718814d269b15b655719232daede9dbff1e71559
yihuaf <fang.yihua.eric@gmail.com>
2020-07-30 13:40:22 -0700

    Refactor python runenv (#725)
    * Refactor python runenv
    
    We now use `/usr/local/lib/python{X,Y}` instead of custome location. No longer
    need to use PYTHONHOME or PYTHONPATH.

--- payloads/python/runenv.dockerfile
+++ payloads/python/runenv.dockerfile
@@ -3,8 +3,5 @@ FROM scratch
 # Python version must be passed obn build
 ARG VERS
 
-ENV PYTHONPATH=/cpython/Lib:/cpython/build/lib.linux-x86_64-${VERS}
-ENV PYTHONHOME=foo:bar
-
 COPY . /
 ENTRYPOINT [ "/usr/local/bin/python3" ]
---
+++ payloads/python/runenv/rootfs.sh
@@ -0,0 +1,86 @@
+#!/bin/bash -e
+#  Copyright © 2018-2020 Kontain Inc. All rights reserved.
+#
+#  Kontain Inc CONFIDENTIAL
+#
+#  This file includes unpublished proprietary source code of Kontain Inc. The
+#  copyright notice above does not evidence any actual or intended publication of
+#  such source code. Disclosure of this source code or any related proprietary
+#  information is strictly prohibited without the express written permission of
+#  Kontain Inc.
+
+# Use this script to create a rootfs for the python runenv image. We try to use
+# as close to default as possible to avoid messing with PYTHONHOME and
+# PYTHONPATH. The layout would be the follow. Using python:3.X-alpine as
+# reference.
+# /opt/kontain/bin/km (mapped into container through volume)
+# /usr/local/bin/python3 -> /opt/kontain/bin/km
+# /usr/local/bin/python3.km
+# /usr/local/lib/python{X,Y} (cpython/Lib)
+# /usr/local/lib/python{X,Y}/lib-dynload (cpython/build/lib.linux-x86_64-${X,Y})
+
+[ "${TRACE}" ] && set -x
+
+readonly PROGNAME=$(basename $0)
+readonly CURRENT=$(readlink -m $(dirname $0))
+
+function usage() {
+    cat <<- EOF
+usage: $PROGNAME <PYTHON SRC> <RUNENV PATH> <PYTHON VERSION>
+
+Script used to create a rootfs for python runenv image.
+
+EOF
+}
+
+if [[ $# != 3 ]]; then
+    usage
+    exit 1
+fi
+
+readonly PYTHON_SRC=$(readlink -m $1)
+readonly RUNENV_PATH=$(readlink -m $2)
+readonly PYTHON_VERSION=$3
+
+readonly RUNENV_PYTHON_BIN=/usr/local/bin
+readonly RUNENV_PYTHON_LIB=/usr/local/lib/python${PYTHON_VERSION}
+readonly RUNENV_PYTHON_LIB_DYNLOAD=${RUNENV_PYTHON_LIB}/lib-dynload
+readonly PYTHON_KM=${PYTHON_SRC}/python.km
+
+readonly PYTHON_SRC_LIB=${PYTHON_SRC}/build/lib.linux-x86_64-${PYTHON_VERSION}
+readonly PYTHON_SRC_LIB_STUBS=$(find ${PYTHON_SRC_LIB} -name '*.so')
+readonly RUNENV_PYTHON_LIB_STUBS=$(echo ${PYTHON_SRC_LIB_STUBS} | sed "s|${PYTHON_SRC_LIB}|${RUNENV_PYTHON_LIB_DYNLOAD}|g")
+
+readonly RUNENV_EXCLUDE='--exclude=*/test --exclude=*/__pycache__ --exclude *.exe --exclude *.whl'
+
+function main() {
+
+    echo "Creating rootfs from ${PYTHON_SRC} inside ${RUNENV_PATH} for python${PYTHON_VERSION}..."
+
+	rm -rf ${RUNENV_PATH} && mkdir -p ${RUNENV_PATH}
+	mkdir -p $(realpath -m ${RUNENV_PATH}/${RUNENV_PYTHON_BIN})
+	mkdir -p $(realpath -m ${RUNENV_PATH}/${RUNENV_PYTHON_LIB_DYNLOAD})
+	mkdir -p ${RUNENV_PATH}/opt/kontain/bin
+
+    echo "Installing km and symlinks to ${RUNENV_PATH} ..."
+    # Install python.km into /usr/local/bin
+    install -s ${PYTHON_KM} $(realpath -m ${RUNENV_PATH}/${RUNENV_PYTHON_BIN}/python3.km)
+    # Create the symlink to be used with km shebang
+	ln -s /opt/kontain/bin/km $(realpath -m ${RUNENV_PATH}/${RUNENV_PYTHON_BIN}/python3)
+
+    echo "Installing module libs..."
+    # Install module libs to /usr/local/lib/python{X,Y}
+	tar -cf - ${RUNENV_EXCLUDE} -C ${PYTHON_SRC}/Lib . | tar -C $(realpath -m ${RUNENV_PATH}/${RUNENV_PYTHON_LIB}) -xf -
+
+    echo "Installing platform libs as thumbstones..."
+    # Install platform specific libs to /usr/lib/python{X,Y}/lib-dynload. The files are thumbstones.
+    for stub in ${RUNENV_PYTHON_LIB_STUBS}
+    do
+        mkdir -p $(realpath -m ${RUNENV_PATH}/$(dirname $stub))
+        touch $(realpath -m ${RUNENV_PATH}/$stub)
+    done
+
+    echo "Done..."
+}
+
+main
