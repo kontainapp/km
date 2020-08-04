ac035ca135a92819aa22d249cbc20a8cbb0e2e2d
Serge <serge@kontain.app>
2020-08-04 12:57:28 -0700

    make argv look like that of guest (#730)
    * make argv look like that of guest, don't virtualize /proc/self/cmdline
    * make fs_exec_test use /proc/self/exe, as cmdline now is not virtualized
    * compare cmdline with argv in cmdline test

--- tests/fs_exec_test.c
+++ tests/fs_exec_test.c
@@ -23,27 +23,22 @@ extern char** environ;
 
 int main(int argc, char** argv)
 {
-   int fd = open("/proc/self/cmdline", O_RDONLY);
+   char* name = "/proc/self/exe";
 
    if (argc > 1 && strcmp(argv[1], "parent") == 0) {
       char parent_buf[256];
-      int parent_rc = read(fd, parent_buf, sizeof(parent_buf));
-      lseek(fd, SEEK_SET, 0);
-      printf("parent cmdline:");
-      for (int i = 0; i < parent_rc;) {
-         i += printf(" %s", parent_buf + i);
+      int parent_rc = readlink(name, parent_buf, sizeof(parent_buf));
+      if (parent_rc > 0) {
+         printf("parent exe: %.*s %s\n", parent_rc, parent_buf, argv[1]);
       }
-      printf("\n");
       char* testargv[] = {"fs_exec_test", "child", NULL};
       int rc = execve("fs_exec_test", testargv, environ);
       printf("exec %d %s\n", rc, strerror(errno));
    } else {
       char buf[256];
-      int child_rc = read(fd, buf, sizeof(buf));
-      printf("child  cmdline:");
-      for (int i = 0; i < child_rc;) {
-         i += printf(" %s", buf + i);
+      int child_rc = readlink(name, buf, sizeof(buf));
+      if (child_rc > 0) {
+         printf("child  exe: %.*s %s\n", child_rc, buf, argv[1]);
       }
-      printf("\n");
    }
 }
