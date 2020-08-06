323b5b2a220e136e2c26ba074dda7dc5cef83417
yihuaf <efang@kontain.app>
2020-08-05 20:24:08 -0400

    add headers

--- tools/faktory/tests/flask/assets/app/app.py
+++ tools/faktory/tests/flask/assets/app/app.py
@@ -1,3 +1,13 @@
+# Copyright © 2020 Kontain Inc. All rights reserved.
+#
+# Kontain Inc CONFIDENTIAL
+#
+# This file includes unpublished proprietary source code of Kontain Inc. The
+# copyright notice above does not evidence any actual or intended publication
+# of such source code. Disclosure of this source code or any related
+# proprietary information is strictly prohibited without the express written
+# permission of Kontain Inc.
+
 # Taken directly from https://flask.palletsprojects.com/en/1.1.x/quickstart/
 
 from flask import Flask
