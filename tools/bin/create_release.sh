457b7cf8c88bc27ff72df2528292359a03f67978
Mark Sterin <msterin@kontain.app>
2020-09-02 14:20:02 -0700

    cleanup of create_release

--- tools/bin/create_release.sh
+++ tools/bin/create_release.sh
@@ -20,18 +20,23 @@ declare -a files ;    files=(.                docs/release tests/hello_test.km b
 
 for i in $(seq 0 $(("${#locations[@]}" - 1)) ) ; do
    source="${locations[$i]}/${files[$i]}"
-   decompress_list=`find $source -type f | xargs file | egrep '(shared|archive|relocatable)' | awk -F: '{print $1}'`
-   if [ -n "$decompress_list" ] ; then echo Decompressing .debug_info for `echo $decompress_list | wc -w` files ; fi
-   for file in $decompress_list
+   decompress_list=$(find $source -type f -size +8b -exec file '{}' ';' |  awk -F: '/(shared|archive|relocatable)/ {print $1}')
+   if [ -n "$decompress_list" ] ; then
+      echo Decompressing .debug_info for `echo $decompress_list | wc -w` files in $source
+      if [ ! -w $source ] ; then
+         echo WARNING: No write access to $source - objcopy will need to touch files.  May need to run as sudo
+      fi
+   fi
+
+   for f in $decompress_list
    do
-      objcopy --decompress-debug-sections --preserve-dates $file
+      objcopy --decompress-debug-sections --preserve-dates $f
    done
+
    echo "Packaging ${source}"
    tar -C ${locations[$i]} -rf $TARBALL ${files[$i]}
 done
 
 echo "Zipping $TARBALL.gz ..."
 gzip $TARBALL
-
-ls -lh ${TARBALL}*
 
