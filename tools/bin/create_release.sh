aedf4520f20b4d3049bb7e612c7c2293c94f4f23
Mark Sterin <msterin@kontain.app>
2020-09-02 15:55:45 -0700

    Release: added_debug_uncompress  and Moved release docs to km-release submodule (#760)
    * added_debug_uncompress
    * Moved release docs to km-release

--- tools/bin/create_release.sh
+++ tools/bin/create_release.sh
@@ -3,6 +3,8 @@
 #
 # Creates a release kontain.tar.gz for uploading to github. To unpackage, 'tar -C /opt/kontain -xvf kontain.tar.gz'
 #
+# May need to run as a `sudo` because objcopy modifies files in /opt/kontain
+#
 [ "$TRACE" ] && set -x
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
@@ -17,12 +19,24 @@ declare -a locations; locations=(/opt/kontain ../..        ../..               .
 declare -a files ;    files=(.                docs/release tests/hello_test.km bin)
 
 for i in $(seq 0 $(("${#locations[@]}" - 1)) ) ; do
-   echo "Packaging ${locations[$i]}/${files[$i]}"
-   eval "tar -C ${locations[$i]} -rf $TARBALL ${files[$i]}"
+   source="${locations[$i]}/${files[$i]}"
+   decompress_list=$(find $source -type f -size +8b -exec file '{}' ';' |  awk -F: '/(shared|archive|relocatable)/ {print $1}')
+   if [ -n "$decompress_list" ] ; then
+      echo Decompressing .debug_info for `echo $decompress_list | wc -w` files in $source
+      if [ ! -w $source ] ; then
+         echo WARNING: No write access to $source - objcopy will need to touch files.  May need to run as sudo
+      fi
+   fi
+
+   for f in $decompress_list
+   do
+      objcopy --decompress-debug-sections --preserve-dates $f
+   done
+
+   echo "Packaging ${source}"
+   tar -C ${locations[$i]} -rf $TARBALL ${files[$i]}
 done
 
 echo "Zipping $TARBALL.gz ..."
 gzip $TARBALL
-
-ls -lh ${TARBALL}*
 
