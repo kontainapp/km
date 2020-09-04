49a1a3d749210fdf6d55c8aa01198a7605507346
Serge Pashenkov <serge@kontain.app>
2020-09-04 15:05:38 -0700

    use absolute path names so vscode find errors easily. Simplify compile and dependencies. Add clean task

--- km/km_trace.c
+++ km/km_trace.c
@@ -99,10 +99,9 @@ void __km_trace(int errnum, const char* function, int linenumber, const char* fm
 
    va_end(ap);
 
-   if (km_log_file != NULL && stderr == NULL) {
+   if (km_log_file != NULL) {
       fputs(traceline, km_log_file);
-   }
-   if (stderr != NULL) {
+   } else if (stderr != NULL) {
       if (km_trace_noniteractive == 1 || km_trace_pid == 0 || km_called_via_exec() == 1) {
          fputs(traceline, stderr);
       } else {
