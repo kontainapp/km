8eb54f86c4c392cabf323cfb3505fe5d0511def3
Paul Popelka <paulp@kontain.app>
2020-09-16 08:46:08 -0700

    Address review comment.

--- make/dep_check.mk
+++ make/dep_check.mk
@@ -12,6 +12,7 @@
 # A target that can be used to verify that needed packages are installed.
 # Define DEP_PACKAGES as a list of needed packages and use .check_packages
 # as a dependecy in a rule.
+# TODO: fix payloads/java/Makefile and payloads/nginx/Makefile to use this.
 
 export define check_packages 
 	echo "Checking that packages ${DEP_PACKAGES} are present"
