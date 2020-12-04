cb4da2c8db55fbc28b0e1d22fab0081674c11681
John Muth <muth@kontain.app>
2020-12-04 13:30:30 -0800

    KM snapshot API for Python 3 (#975)
    * KM snapshot API for Python 3
    
    * test for python API
    
    * make names conform to M1 spec
    
    * add test_snapshot.py to python testenv
    
    * update python buildenv
    
    * Remove bad copy (breaks kkm)

--- payloads/python/scripts/test-run.sh
+++ payloads/python/scripts/test-run.sh
@@ -9,3 +9,7 @@ PYTHON=${1:-Please_pass_python_interpreter}
 ${PYTHON} ./scripts/sanity_test.py
 ${PYTHON} ./test_unittest.py
 ${PYTHON} ./cpython/Lib/unittest/test/
+rm -f kmsnap
+${PYTHON} ./test_snapshot.py
+[ ! -f kmsnap ] && echo No test_snapshot
+rm -f kmsnap
