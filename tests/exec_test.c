34099ae8cffc0169f2b678c07d44eb26f784f88f
Mark Sterin <msterin@kontain.app>
2021-03-30 13:23:08 -0700

    Correctly hande .km files passed to exec() in KM (#1101)
    Also:
    * Remove trailing quote in exec args parsing
    * Review fixes and extra tests

--- tests/exec_test.c
+++ tests/exec_test.c
@@ -10,21 +10,29 @@
  * permission of Kontain Inc.
  */
 
+#include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include "mmap_test.h"   // for KM_PAYLOAD definition
 
 /*
  * A simple program to test execve() and execveat() (fexecve()) by exec to "print_argenv_test"
  *
- * Use the -f flag to test fexecve().
- * Use the -e flag to test ENOENT errno
- * Set KM_EXEC_TEST_EXE environment to override what it being exec-ed into
+ * Flags:
+ * -f to test fexecve().
+ * -e to test ENOENT errno
+ * -k flags to test exec into '.km' file
+ * -s to tests /bin/sh parse
+ * -X to test exec into realpath of /proc/self/exe
+ *
+ * Set KM_EXEC_TEST_EXE environment to override what it being exec-ed into by default
  *
  * TODO: execveat() is only tested for fexecve() subset, need to add test
  *
@@ -32,6 +40,8 @@
 
 #define EXEC_TEST_EXE_ENV "KM_EXEC_TEST_EXE"
 #define EXEC_TEST_EXE_DEFAULT "print_argenv_test"
+#define EXEC_TEST_EXE_KM "hello_test.km"
+
 #define EXEC_TEST_EXE_ENOENT "this_file_should_not_exist.ever"
 
 int main(int argc, char** argv)
@@ -45,7 +55,13 @@ int main(int argc, char** argv)
    char* testargv[] = {exec_test, "a1", "b2", "c3", "d4", NULL};
    char* testenvp[] = {"ONE=one", "TWO=two", "THREE=three", "FOUR=four", NULL};
 
-   if (argc == 2 && strcmp(argv[1], "-f") == 0) {
+   if (argc == 1) {
+      rc = execve(exec_test, testargv, testenvp);
+      fprintf(stderr, "execve() failed, rc %d, errno %d, %s\n", rc, errno, strerror(errno));
+      return 99;
+   }
+
+   if (strcmp(argv[1], "-f") == 0) {
       int exefd = open(exec_test, O_RDONLY);
       if (exefd < 0) {
          fprintf(stderr, "open() of %s failed, %s\n", exec_test, strerror(errno));
@@ -55,13 +71,50 @@ int main(int argc, char** argv)
          fprintf(stderr, "fexecve() failed, errno %d, %s\n", errno, strerror(errno));
          close(exefd);
       }
-   } else if (argc == 2 && strcmp(argv[1], "-e") == 0) {
+   } else if (strcmp(argv[1], "-e") == 0) {
       rc = execve(EXEC_TEST_EXE_ENOENT, testargv, testenvp);
-      fprintf(stderr, "execve() rc %d, errno %d, %s: %s\n", rc, errno, strerror(errno), EXEC_TEST_EXE_ENOENT);
+      fprintf(stderr,
+              "Expected failure: execve() rc %d, errno %d, %s: %s\n",
+              rc,
+              errno,
+              strerror(errno),
+              EXEC_TEST_EXE_ENOENT);
       return rc;
-   } else {
-      rc = execve(exec_test, testargv, testenvp);
+   } else if (strcmp(argv[1], "-k") == 0) {   // exec into '.km' file
+      if (KM_PAYLOAD() == 0) {
+         fprintf(stderr, "not in payload - nothing to do here\n");
+         return 0;
+      }
+      char* testargv[] = {EXEC_TEST_EXE_KM, "TESTING exec to .km", "passing newline\n!", NULL};
+      rc = execve(EXEC_TEST_EXE_KM, testargv, testenvp);
       fprintf(stderr, "execve() failed, rc %d, errno %d, %s\n", rc, errno, strerror(errno));
+   } else if (strcmp(argv[1], "-X") == 0) {   // exec into /proc/self/exe
+      char* testargv[] = {"/proc/self/exe", "-0", NULL};
+      char buf[PATH_MAX];
+      char* path = realpath(testargv[0], buf);
+      assert(path != NULL);
+      fprintf(stderr, "%s resolved to %s\n", testargv[0], path);
+
+      rc = execve(path, testargv, testenvp);
+      fprintf(stderr, "execve() failed, rc %d, errno %d, %s\n", rc, errno, strerror(errno));
+   } else if (strcmp(argv[1], "-s") == 0) {   // exec into /bin/sh
+      if (KM_PAYLOAD() == 0) {
+         fprintf(stderr, "not in payload - nothing to do here\n");
+         return 0;
+      }
+      // sanity check for /bin/sh parse. Note that it works ONLY for .km pass
+      char* testargv[] = {"/bin/sh",
+                          "-c",
+                          "./hello_test.km --parse \"string with quotes\" 'more\\ quotes' ! ;",
+                          NULL};
+      rc = execve("/bin/sh", testargv, testenvp);
+      fprintf(stderr, "execve() failed, rc %d, errno %d, %s\n", rc, errno, strerror(errno));
+   } else if (strcmp(argv[1], "-0") == 0) {   // noop, usually from exec-d program
+      fprintf(stderr, "noop: -0 requested\n");
+      return 0;
+   } else {
+      fprintf(stderr, "wrong flag %s\n", argv[1]);
+      return 88;
    }
 
    return 99;
