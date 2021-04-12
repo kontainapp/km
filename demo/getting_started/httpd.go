c4846cc81c943466057fe865e6592d7236e7edd9
Mark Sterin <msterin@kontain.app>
2021-04-12 11:08:34 -0700

    Enabled a bunch of Node tests than now pass  (#1107)
    * Enabled a bunch of Node tests than now pass
    
    Also, opportunistically, added a few more lines to httpd.go demo - was using it during GO debug tries
    
    tested with 'make -C payloads/node test-all' , and with nightly

--- demo/getting_started/httpd.go
+++ demo/getting_started/httpd.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
-    fmt.Fprintf(os.Stdout, "Starting on localhost:8080 ...\n")
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":8080", nil)
+	fmt.Fprintf(os.Stdout, "Starting on localhost:8080 ...\n")
+	http.HandleFunc("/bye", ByeServer)
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello World\n")
+	fmt.Fprintf(w, "Hello World\n")
+}
+
+func ByeServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bye World\n")
 }
