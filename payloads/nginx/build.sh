dcf9c14bbc79c6afcb8407d2f1d17f4e19f77b6c
Mark Sterin <msterin@kontain.app>
2021-04-21 19:19:04 -0700

    Updated nginx build to use the latest, and actually build
    Not tested (other than basic invocation) yet, just getting the build fixed

--- payloads/nginx/build.sh
+++ payloads/nginx/build.sh
@@ -1,45 +1,30 @@
 #!/bin/bash
-set -e; [ "$TRACE" ] && set -x
+set -e
+[ "$TRACE" ] && set -x
 
 TOP=$(git rev-parse --show-toplevel)
-CURRENT=${TOP}/payloads/nginx
-NGINX_TOP=${CURRENT}/nginx
+NGINX_TOP=${TOP}/payloads/nginx/nginx
 
 BUILDENV_IMAGE_NAME="kontain/buildenv-nginx-alpine"
 
-if [ ! -d ${NGINX_TOP} ]
-then
-    echo "${NGINX_TOP} doesn't exist. Need to run make fromsrc"
-    exit 1
+if [ ! -d ${NGINX_TOP} ]; then
+   echo "${NGINX_TOP} doesn't exist. Need to run make fromsrc"
+   exit 1
 fi
 
-if [ -z $(docker image ls -q ${BUILDENV_IMAGE_NAME}) ]
-then
-    docker build -t ${BUILDENV_IMAGE_NAME} -f buildenv.dockerfile ${CURRENT}
+if [ -z $(docker image ls -q ${BUILDENV_IMAGE_NAME}) ]; then
+   make buildenv-image
 fi
-
-BUILDENV_CONTAINER_NAME="kontain-nginx-payload-build"
 
 # Clean up any running build container. This will only happen if the previous
 # build process is interrupted.
+BUILDENV_CONTAINER_NAME="kontain-nginx-payload-build"
 if [ "$(docker ps -a -q -f name=${BUILDENV_CONTAINER_NAME})" ]; then
-    docker rm -f ${BUILDENV_CONTAINER_NAME}
+   docker rm -f ${BUILDENV_CONTAINER_NAME}
 fi
 
-docker run --rm \
-    -it \
-    -d \
-    -v ${NGINX_TOP}:/nginx:Z \
-    -w /nginx \
-    --name ${BUILDENV_CONTAINER_NAME} \
-    -u $(id -u ${USER}):$(id -g ${USER}) \
-    ${BUILDENV_IMAGE_NAME}
-
-docker exec -w /nginx ${BUILDENV_CONTAINER_NAME} \
-    ./auto/configure \
-    --prefix=/opt/kontain/nginx
-
-docker exec -w /nginx ${BUILDENV_CONTAINER_NAME} \
-    make -j $(nproc)
-
-docker stop ${BUILDENV_CONTAINER_NAME}
+docker run -it --name ${BUILDENV_CONTAINER_NAME} \
+   -v ${NGINX_TOP}:/nginx:Z -w /nginx \
+   -u $(id -u ${USER}):$(id -g ${USER}) \
+   ${BUILDENV_IMAGE_NAME} \
+   sh -c "./auto/configure --prefix=/opt/kontain/nginx &&  make -j $(nproc)"
