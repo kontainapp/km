8a71fd2fcf2d1beef3b4563d97165f4d652f49ac
Mark Sterin <msterin@kontain.app>
2021-04-23 15:45:51 -0700

    Implementation of  dlopen(NULL) for python. Also fixes python -m debupy (#1162)
    * Implements dlopen(NULL) for python and fixes python -m debupy
    
    Fixes #1152 (Python: failure in debugging)
    
    Partially fixes dlopen(NULL) - for Python only. Python is hardcoded there,
    which is not very good - I am open to ideas on how to be more creative
    
    * syntax sugar

--- payloads/python/link-km.sh
+++ payloads/python/link-km.sh
@@ -2,10 +2,11 @@
 #
 # Links Python.km from python libs and km 'dlstatic' builditns mentioned in $target/linkline_km.txt
 #
-set -e ; [ "$TRACE" ] && set -x
+set -e
+[ "$TRACE" ] && set -x
 
-if [ "$1" == "--help" ] ; then
-  cat << EOF
+if [ "$1" == "--help" ]; then
+   cat <<EOF
 Link cpython.km:
 
 link.km location target extras
@@ -30,20 +31,20 @@ KM_OPT_BIN=/opt/kontain/bin
 
 cd $OUT
 kontain-gcc -pthread -ggdb ${BUILD}/Programs/python.o \
-   @linkline_km.txt ${EXTRA_FILES} \
+   @linkline_km.txt ${EXTRA_FILES} ${OUT}/python.km.symbols.o \
    ${BUILD}/libpython3*.a -lz -lssl -lcrypto -lsqlite3 $LDLIBS \
    -o ${NAME} && chmod a-x ${NAME} && echo Linked: ${OUT}/${NAME}
 
 kontain-gcc -dynamic -Xlinker -export-dynamic -pthread -ggdb ${BUILD}/Programs/python.o \
-    -Xlinker --undefined=strtoull_l \
+   -Xlinker --undefined=strtoull_l \
    ${BUILD}/libpython3*.a -lsqlite3 $LDLIBS \
    -o ${NAME}d
 
 # Add python->km symlink and make python to looking for libs in correct place
 # We want it to work in containers and dev boxes, so locking to /opt/kontain and
 # making KM_BLDDIR placement optional
-rm -f python ; ln -s ${KM_OPT_BIN}/km python
-cat > ${KM_OPT_BIN}/pyvenv.cfg <<- EOF
+ln -sf ${KM_OPT_BIN}/km python
+cat >${KM_OPT_BIN}/pyvenv.cfg <<-EOF
 home = ${OUT}
 include-system-site-packages = false
 version = ${PYTHON_VERSION}
@@ -51,6 +52,6 @@ runtime = kontain
 runtime_branch = ${SRC_BRANCH}
 runtime_sha = ${SRC+SHA}
 EOF
-if [ -d "${KM_BLDDIR}" ] ; then
+if [ -d "${KM_BLDDIR}" ]; then
    cp ${KM_OPT_BIN}/pyvenv.cfg ${KM_BLDDIR}
 fi
