aed3de03b4e923b1b448e3778a498df86116ae39
John Muth <muth@kontain.app>
2021-05-02 08:52:11 -0700

    Python bindings for KM snapshot getdata/putdata. For Faas.

--- payloads/python/km_api.c
+++ payloads/python/km_api.c
@@ -15,6 +15,7 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #define __GNU_SOURCE
+#include <errno.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 
@@ -34,11 +35,57 @@ static PyObject* kontain_snapshot_take(PyObject* self, PyObject* args, PyObject*
    return Py_None;
 }
 
+static PyObject* kontain_snapshot_getdata(PyObject* self, PyObject* args, PyObject* keywds)
+{
+   int bufsz = 4096;
+   PyGILState_STATE state = PyGILState_Ensure();
+   void* buf = PyObject_Malloc(bufsz);
+   PyGILState_Release(state);
+   int ret;
+
+   // 504 = km_snapshot_getdata
+   if ((ret = syscall(504, buf, bufsz)) < 0) {
+      return NULL;
+   }
+
+   PyObject* obj = Py_BuildValue("s#", buf, ret);
+
+   return obj;
+}
+
+static PyObject* kontain_snapshot_putdata(PyObject* self, PyObject* args, PyObject* keywds)
+{
+   char* buf = NULL;
+   if (!PyArg_ParseTuple(args, "s", &buf)) {
+      printf("fail1\n");
+      return NULL;
+   }
+   int bufsz = strlen(buf);
+   printf("buf=%p bufsz=%d %s\n", buf, bufsz, buf);
+
+   // 503 = km_snapshot_putdata
+   int ret;
+   if ((ret = syscall(503, buf, bufsz)) < 0) {
+      printf("fail2 - ret=%d errno=%d\n", ret, errno);
+      return NULL;
+   }
+
+   return Py_None;
+}
+
 static PyMethodDef KontainMethods[] = {
     {.ml_name = "take",
      .ml_meth = kontain_snapshot_take,
      .ml_flags = METH_VARARGS | METH_KEYWORDS,
      .ml_doc = "Take process snapshot"},
+    {.ml_name = "getdata",
+     .ml_meth = kontain_snapshot_getdata,
+     .ml_flags = METH_VARARGS | METH_KEYWORDS,
+     .ml_doc = "Read snapshot start data from KM"},
+    {.ml_name = "putdata",
+     .ml_meth = kontain_snapshot_putdata,
+     .ml_flags = METH_VARARGS | METH_KEYWORDS,
+     .ml_doc = "Write snapshot completion data"},
     {} /* Sentinel */
 };
 
