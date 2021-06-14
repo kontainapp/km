6f8270ca34cdc4fa7511391e797ab7b163de42dc
Mark Sterin <msterin@kontain.app>
2021-06-13 21:10:16 -0700

    Reworked KKM tests workflow  (#1248)
    It now runs in ~5 min (instead of ~25-30 before the PR). Code is now simpler/easier to support and half the size.
    
    Changes:
    
    - use Packer for all AWS interactions
    - drop a build on AWS and instead pull testenv
    - use larger AWS instance (now that it take less time, it costs us less)

--- cloud/aws/kkm-test.bash
+++ cloud/aws/kkm-test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 #
 # Copyright © 2020 Kontain Inc. All rights reserved.
 #
@@ -10,108 +10,48 @@
 #  information is strictly prohibited without the express written permission of
 #  Kontain Inc.
 #
-# script to run km tests with kkm in aws
+# Script to run km tests with kkm in aws.
+#
+# Expects to run as Packer provisioner, and some var (e.g. IMAGE_VERSION and auth-related)
+# are expected to be pre-set
 
 [ "$TRACE" ] && set -x
 
-set -x
-
-export TERM=linux
-ROOT_DIR="${HOME}/src/"
-LOG_DIR="${ROOT_DIR}/log/"
-
+# note: it timestamp is needed, pipe the script output
+# via `ts` (from `moreutils`) or turn on  CI timestamps via UI
 function log_message {
-   time_now=$(date +"%T-%N")
-   echo -e $time_now:$@
+   echo -e "=== $@"
 }
 
+trap error_exit EXIT
 function error_exit {
-   touch ${LOG_DIR}/FAILED
-   sudo dmesg -c > ${LOG_DIR}/kernel-run-logs
-   log_message $1
-   exit 0
+   log_message "Kernel logs:"
+   dmesg
 }
 
 
-if [ $# -ne 1 ]
-then
-   error_exit "Not enough parameters\nUsage:\n\t$0 branch"
-fi
-
-BRANCH_NAME=$1
-
-umask 0
-sudo chmod 0777 /opt/kontain/
-DIRLIST="/opt/kontain/bin/ /opt/kontain/runtime/ ${ROOT_DIR} ${LOG_DIR}"
-for entry in ${DIRLIST}
-do
-   sudo rm -fr $entry
-   sudo mkdir -m 0777 -p $entry
-done
-
+ROOT_DIR="${HOME}/src/"
+mkdir -p ${ROOT_DIR}
 cd ${ROOT_DIR}
 
-git clone --branch ${BRANCH_NAME} --recurse-submodules git@github.com:kontainapp/km.git >& ${LOG_DIR}/git-clone
-if [ $? -ne 0 ]
-then
-   error_exit "Failed to clone repository"
-fi
-log_message "KM repo clone success"
-
+# we only need KKM repo and Make system so not pulling in all submodules
+echo Cloning KM repo. branch: ${SOURCE_BRANCH?undefined}
+git clone --branch ${SOURCE_BRANCH} git@github.com:kontainapp/km.git
 cd km
+git submodule update --init kkm
 
+log_message "Build, install and unit test KKM"
+# Build has to happen here since it's kernel specific
+make -C kkm/kkm
+make -C kkm/test_kkm
+sudo dmesg --clear
+sudo insmod kkm/kkm/kkm.ko
+./kkm/test_kkm/test_kkm
+
+log_message "Pull testenv-image:${IMAGE_VERSION} and test with docker"
 make -C cloud/azure login-cli
-make pull-buildenv-image
-
-log_message "starting build"
-BUILD_DIRS="kkm/kkm kkm/test_kkm . payloads/python payloads/node payloads/java"
-for builddir in $BUILD_DIRS
+for dir in tests payloads
 do
-   echo "################## build logs for $builddir" &>> ${LOG_DIR}/build-all
-   make -C $builddir &>> ${LOG_DIR}/build-all
-   log_message "$builddir build complete"
+   make -C $dir pull-testenv-image
+   make -C $dir test-withdocker HYPERVISOR_DEVICE=/dev/kkm
 done
-
-EXEC_LIST="kkm/kkm/kkm.ko kkm/test_kkm/test_kkm build/km/km"
-for execfile in $EXEC_LIST
-do
-   if [ ! -f $execfile ]
-   then
-      error_exit "cannot find $execfile file"
-   fi
-done
-
-# clear kernel logs
-sudo dmesg -c > ${LOG_DIR}/kernel-boot-logs
-
-echo "################## run logs for kkm" &>> ${LOG_DIR}/run-all
-sudo insmod kkm/kkm/kkm.ko &>> ${LOG_DIR}/run-all
-if [ ! -e /dev/kkm ]
-then
-   error_exit "cannot find /dev/kkm"
-fi
-log_message "KKM module init success"
-
-TEST_DIRS="tests payloads/python payloads/node payloads/java"
-log_message "starting test"
-for testdir in $TEST_DIRS
-do
-   echo "################## run logs for $testdir tests" &>> ${LOG_DIR}/run-all
-   make -C $testdir test USEVIRT=kkm &>> ${LOG_DIR}/run-all
-   if [ $? -ne 0 ]
-   then
-      error_exit "$testdir tests failed"
-   fi
-   log_message "$testdir test complete"
-done
-
-sudo dmesg -c > ${LOG_DIR}/kernel-run-logs
-grep "0 failures" ${LOG_DIR}/run-all >& /dev/null
-if [ $? -ne 0 ]
-then
-   error_exit "tests failed"
-fi
-touch ${LOG_DIR}/PASSED
-log_message "tests successfull"
-
-exit 0
