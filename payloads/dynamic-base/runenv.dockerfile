817ae1cf688e47ce43b0dc606c4f27fdae37e129
Serge Pashenkov <serge@kontain.app>
2021-06-17 16:42:36 -0700

    use mimalloc with node and java

--- payloads/dynamic-base/runenv.dockerfile
+++ payloads/dynamic-base/runenv.dockerfile
@@ -1,4 +1,5 @@
 ARG RUNENV_IMAGE_VERSION=latest
 
 FROM kontainapp/runenv-busybox:${RUNENV_IMAGE_VERSION}
-ADD runtime /opt/kontain/runtime/
+ADD --chown=0:0 runtime /opt/kontain/runtime/
+ADD --chown=0:0 lib /opt/kontain/lib/
--- payloads/java/link-km.sh
+++ payloads/java/link-km.sh
@@ -19,5 +19,6 @@ ${KM_TOP}/tools/bin/kontain-gcc -dynamic -rdynamic \
     -Wl,-O1 -m64 -Wl,--allow-shlib-undefined -Wl,--exclude-libs,ALL \
     ${JDK_DIR}/support/native/java.base/java/main.o \
     -L${JDK_DIR}/images/jdk/lib/jli -ljli \
+    -L /opt/kontain/lib -lmimalloc \
     -o ${OUT_DIR}/java.kmd
 
