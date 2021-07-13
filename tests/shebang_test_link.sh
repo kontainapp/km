cd5d9687ce90e0a6b22fcfffc08cf68c4cebbb97
sv641 <svetsa@kontain.app>
2021-07-13 16:50:28 -0700

    new license (#1292)
    * Change licensing model to Apache License Version 2.0

--- tests/shebang_test_link.sh
+++ tests/shebang_test_link.sh
@@ -1,5 +1,21 @@
 #!hello_test_link arguments to test, should be one
 #
+# Copyright 2021 Kontain Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+#
 # Only the first line is looked at when passed to KM as a payload file
 # The rest are ignored for now, but in the future we may add here config info
 # or some build-in fun,  e.g.
