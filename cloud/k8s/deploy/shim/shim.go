7f8a957bb012afe2979cfec61cc0a919071d68e1
John Muth <muth@kontain.app>
2021-10-12 17:06:25 -0700

    Fix path name a tweak makefiles

--- cloud/k8s/deploy/shim/shim.go
+++ cloud/k8s/deploy/shim/shim.go
@@ -75,7 +75,7 @@ func (s *service) Cleanup(ctx context.Context) (*taskAPI.DeleteResponse, error) 
 func (s *service) Create(ctx context.Context, r *taskAPI.CreateTaskRequest) (_ *taskAPI.CreateTaskResponse, err error) {
 
 	r.Options, err = typeurl.MarshalAny(&runc_opts.Options{
-		BinaryName: "krun",
+		BinaryName: "/opt/bin/krun",
 	})
 	return s.parent.Create(ctx, r)
 }
