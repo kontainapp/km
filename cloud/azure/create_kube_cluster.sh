d7ced8eb614bde29bd1929632477a37e2dae44b1
Serge Pashenkov <serge@kontain.app>
2021-11-02 16:17:15 -0700

    make create_kube_cluster.sh work again

--- cloud/azure/create_kube_cluster.sh
+++ cloud/azure/create_kube_cluster.sh
@@ -31,15 +31,13 @@ appPwd=`az ad sp create-for-rbac --name ${K8_SERVICE_PRINCIPAL} --skip-assignmen
 echo Do not lose it PWD $appPwd . TODO: switch to pre-generated PEM certs
 [ "$TRACE" ] && set -x
 acrId=`az acr show --resource-group ${CLOUD_RESOURCE_GROUP} --name ${REGISTRY_NAME} --query "id" --output tsv`
-sp_id=`az ad sp list  --query "[?contains(servicePrincipalNames,'$K8_SERVICE_PRINCIPAL')].{Id: objectId}" --all --output tsv`
-appId=`az ad sp show --id ${sp_id}  --query appId --output tsv`
 sleep 5 # give azure time to propagate the data. TODO - drop 'sleep' and make it wait on whatever is needed here
-az role assignment create --assignee ${appId} --scope ${acrId} --role acrpull --output ${out_type}
+az role assignment create --assignee ${SP_APPID} --scope ${acrId} --role acrpull --output ${out_type}
 
 az aks create --resource-group ${CLOUD_RESOURCE_GROUP}  --name ${K8S_CLUSTER}  \
    --node-vm-size ${K8S_NODE_INSTANCE_SIZE} --node-count 1 \
-   --service-principal ${appId} --client-secret ${appPwd} \
-    --generate-ssh-keys --output ${out_type}
+   --service-principal ${SP_APPID} --client-secret ${SP_PASSWORD} \
+   --generate-ssh-keys --output ${out_type}
 
 # populate kubectl context for K8S_CLUSTER
 # Note: if kubectl is not installed, azure has a helper 'az aks install-cli'
