55f282cc84bb578e351b8144fb12f0e4c2acf015
smijar <1569610+smijar@users.noreply.github.com>
2022-02-03 11:52:02 -0600

    sm/amzn-1481 (#1500)
    * #1481- added amzn to list of supported distros we can install to
    
    * added support for install on amazon linux 2

--- container-runtime/podman_config.sh
+++ container-runtime/podman_config.sh
@@ -27,7 +27,7 @@ set -e ; [ "$TRACE" ] && set -x
 
 # Set variables to know what kind of system this is.
 . /etc/os-release
-[ "$ID" != "fedora" -a "$ID" != "ubuntu" ] && echo "Unsupported linux distribution: $ID" && exit 1
+[ "$ID" != "fedora" -a "$ID" != "ubuntu" -a "$ID" != "amzn" ] && echo "Unsupported linux distribution: $ID" && exit 1
 
 # Some programs we care about
 KRUN_PATH=/opt/kontain/bin/krun
@@ -67,74 +67,74 @@ readonly UNINSTALL_FEDORA_PACKAGES="dnf remove -y "
 # A small function to keep trying apt-get until other apt-get'ers finish.
 function persistent-apt-get
 {
-   eflag=0
-   if echo $- | grep -q "e"; then eflag=1; fi
-
-   MESSAGE="E: Could not get lock /var/lib/dpkg/lock-frontend."
-   APT_GET_OUTPUT=/tmp/apt-get-out-$$
-   while true; do
-      cp /dev/null $APT_GET_OUTPUT
-      [ $eflag -ne 0 ] && set +e
-      apt-get $* >$APT_GET_OUTPUT 2>&1
-      rv=$?
-      [ $eflag -ne 0 ] && set -e
-      if test $rv -ne 0; then
-         if grep -q "$MESSAGE" $APT_GET_OUTPUT; then
-            # Another apt-get is running, try again.
-            sleep 1
-            continue
-         fi
-      fi
-      break
-   done
-   cat $APT_GET_OUTPUT
-   rm -f $APT_GET_OUTPUT
-   return $rv
+    eflag=0
+    if echo $- | grep -q "e"; then eflag=1; fi
+    
+    MESSAGE="E: Could not get lock /var/lib/dpkg/lock-frontend."
+    APT_GET_OUTPUT=/tmp/apt-get-out-$$
+    while true; do
+        cp /dev/null $APT_GET_OUTPUT
+        [ $eflag -ne 0 ] && set +e
+        apt-get $* >$APT_GET_OUTPUT 2>&1
+        rv=$?
+        [ $eflag -ne 0 ] && set -e
+        if test $rv -ne 0; then
+            if grep -q "$MESSAGE" $APT_GET_OUTPUT; then
+                # Another apt-get is running, try again.
+                sleep 1
+                continue
+            fi
+        fi
+        break
+    done
+    cat $APT_GET_OUTPUT
+    rm -f $APT_GET_OUTPUT
+    return $rv
 }
 
 # UNINSTALL
 if [ $# -eq 1 -a "$1" = "-u" ]; then
-   echo "Removing kontain related podman config changes"
-   # put back their containers.conf file
-   if [ -e $HOME_CONTAINERS_CONF.kontainsave ]; then
-      cp $HOME_CONTAINERS_CONF.kontainsave $HOME_CONTAINERS_CONF
-      rm -f $HOME_CONTAINERS_CONF.kontainsave
-   else
-      # No .kontainsave file, so they didn't have one before we got here
-      rm -f $HOME_CONTAINERS_CONF
-   fi
-
-   # remove our selinux policy
-   if [ "$ID" = "fedora" ]; then
-      semodule --remove=$KONTAIN_SELINUX_POLICY
-      # Remove the POLDIR?
-      #rm -fr $POLDIR
-      # We don't restore km's selinux context.
-   fi
-
-   # remove podman and selinux-policy-devel packages
-   # We don't remove the packages needed by krun since many of them are usually present by default.
-   UNINSTALL=UNINSTALL_${ID^^}_PACKAGES
-   PACKAGES=CLEAN_${ID^^}_PACKAGES
-   ${!UNINSTALL} ${!PACKAGES}
-
-   exit 0
+    echo "Removing kontain related podman config changes"
+    # put back their containers.conf file
+    if [ -e $HOME_CONTAINERS_CONF.kontainsave ]; then
+        cp $HOME_CONTAINERS_CONF.kontainsave $HOME_CONTAINERS_CONF
+        rm -f $HOME_CONTAINERS_CONF.kontainsave
+    else
+        # No .kontainsave file, so they didn't have one before we got here
+        rm -f $HOME_CONTAINERS_CONF
+    fi
+    
+    # remove our selinux policy
+    if [ "$ID" = "fedora" ]; then
+        semodule --remove=$KONTAIN_SELINUX_POLICY
+        # Remove the POLDIR?
+        #rm -fr $POLDIR
+        # We don't restore km's selinux context.
+    fi
+    
+    # remove podman and selinux-policy-devel packages
+    # We don't remove the packages needed by krun since many of them are usually present by default.
+    UNINSTALL=UNINSTALL_${ID^^}_PACKAGES
+    PACKAGES=CLEAN_${ID^^}_PACKAGES
+    ${!UNINSTALL} ${!PACKAGES}
+    
+    exit 0
 fi
 
 # return success if selinux is present and enabled, otherwise return fail
 function selinux_is_enabled()
 {
-   # If the "if" statement below is coded this way:
-   # [ -e "${GETENFORCEPATH}" ] && [ "`${GETENFORCEPATH}`" != "Disabled" ]
-   # the shell will complete the command line (including the `progname` stuff before the
-   # test ("[") operands are evaluated.  If the getenforce program is missing then the
-   # function fails because of that.
-   if [ -e "${GETENFORCEPATH}" ]; then
-      if [ "`${GETENFORCEPATH}`" != "Disabled" ]; then
-         return 0
-      fi
-   fi
-   return 1
+    # If the "if" statement below is coded this way:
+    # [ -e "${GETENFORCEPATH}" ] && [ "`${GETENFORCEPATH}`" != "Disabled" ]
+    # the shell will complete the command line (including the `progname` stuff before the
+    # test ("[") operands are evaluated.  If the getenforce program is missing then the
+    # function fails because of that.
+    if [ -e "${GETENFORCEPATH}" ]; then
+        if [ "`${GETENFORCEPATH}`" != "Disabled" ]; then
+            return 0
+        fi
+    fi
+    return 1
 }
 
 # docker-init is in different directories for different distributions
@@ -150,10 +150,10 @@ REFRESH=REFRESH_${ID^^}_PACKAGES
 # Tell ubuntu about the ppackage repo containing podman
 if test "$ID" = "ubuntu"
 then
-   persistent-apt-get update
-   persistent-apt-get install -y wget gnupg2
-   echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-   wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | apt-key add -
+    persistent-apt-get update
+    persistent-apt-get install -y wget gnupg2
+    echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+    wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | apt-key add -
 fi
 # install podman, selinux utils, krun dependencies
 ${!REFRESH}
@@ -161,14 +161,14 @@ ${!INSTALL} ${!PACKAGES}
 
 # Copy whatever they have even if we don't alter it.
 if [ -e $HOME_CONTAINERS_CONF ]; then
-   cp $HOME_CONTAINERS_CONF $HOME_CONTAINERS_CONF.kontainsave
+    cp $HOME_CONTAINERS_CONF $HOME_CONTAINERS_CONF.kontainsave
 fi
 
 # If the user's containers.conf is missing give them a bare bones version
 echo
 if [ ! -e $HOME_CONTAINERS_CONF ]
 then
-   echo "$HOME_CONTAINERS_CONF does not exist, creating it"
+    echo "$HOME_CONTAINERS_CONF does not exist, creating it"
    cat <<EOF >$HOME_CONTAINERS_CONF
 [containers]
 init_path = "$DOCKER_INIT"
@@ -182,65 +182,65 @@ krun = [
         "$KRUN_PATH"
 ]
 EOF
-   if test "$SUDO_USER" != ""
-   then
-      # kludge! fixup owner and group of each component of .config/containers/containers.conf
-      # Yes, "chown -R u:g ~/.config" would do it but there can be lots of stuff in ~/.config
-      # and I don't want to hit all of those directories and their contents.
-      t=$HOME_CONTAINERS_CONF
-      ug=`id -u $SUDO_USER`:`id -g $SUDO_USER`
-      chown $ug $t
-      t=`dirname $t`
-      chown $ug $t
-      t=`dirname $t`
-      chown $ug $t
-   fi
+    if test "$SUDO_USER" != ""
+    then
+        # kludge! fixup owner and group of each component of .config/containers/containers.conf
+        # Yes, "chown -R u:g ~/.config" would do it but there can be lots of stuff in ~/.config
+        # and I don't want to hit all of those directories and their contents.
+        t=$HOME_CONTAINERS_CONF
+        ug=`id -u $SUDO_USER`:`id -g $SUDO_USER`
+        chown $ug $t
+        t=`dirname $t`
+        chown $ug $t
+        t=`dirname $t`
+        chown $ug $t
+    fi
 else
-   # containers.conf does exist, fix its contents if needed.
-   # set init_path in the [containers] section of ~/.config/containers/containers.conf
-   echo "$HOME_CONTAINERS_CONF does exist, checking its contents"
-   if ! grep -q "^ *init_path" $HOME_CONTAINERS_CONF
-   then
-      # no init_path, use the docker init
-      sed --in-place -e "/^\[containers\]/ainit_path = \"$DOCKER_INIT\"" $HOME_CONTAINERS_CONF
-   else
-      if ! grep -q "^ *init_path *= *\"$DOCKER_INIT\"" $HOME_CONTAINERS_CONF
-      then
-         echo "Leaving init_path statement in $HOME_CONTAINERS_CONF unchanged, you may need to change it to $DOCKER_INIT"
-         echo "Existing init_path:"
-         grep "^ *init_path" $HOME_CONTAINERS_CONF
-      else
-         echo "init_path = \"$DOCKER_INIT\"" is already in $HOME_CONTAINERS_CONF
-      fi
-   fi
-
-   # add krun to ~/.config/containers/containers.conf at the beginning of the [engine.runtimes] section
-   echo
-   if ! grep -q "^ *krun =" $HOME_CONTAINERS_CONF
-   then
-      sed --in-place -e "/^\[engine.runtimes\]/akrun = [\n   \"$KRUN_PATH\",\n]\n" $HOME_CONTAINERS_CONF
-   else
-      echo "runtime krun already configured in $HOME_CONTAINERS_CONF"
-      grep -A 3 "^ *krun =" $HOME_CONTAINERS_CONF
-   fi
+    # containers.conf does exist, fix its contents if needed.
+    # set init_path in the [containers] section of ~/.config/containers/containers.conf
+    echo "$HOME_CONTAINERS_CONF does exist, checking its contents"
+    if ! grep -q "^ *init_path" $HOME_CONTAINERS_CONF
+    then
+        # no init_path, use the docker init
+        sed --in-place -e "/^\[containers\]/ainit_path = \"$DOCKER_INIT\"" $HOME_CONTAINERS_CONF
+    else
+        if ! grep -q "^ *init_path *= *\"$DOCKER_INIT\"" $HOME_CONTAINERS_CONF
+        then
+            echo "Leaving init_path statement in $HOME_CONTAINERS_CONF unchanged, you may need to change it to $DOCKER_INIT"
+            echo "Existing init_path:"
+            grep "^ *init_path" $HOME_CONTAINERS_CONF
+        else
+            echo "init_path = \"$DOCKER_INIT\"" is already in $HOME_CONTAINERS_CONF
+        fi
+    fi
+    
+    # add krun to ~/.config/containers/containers.conf at the beginning of the [engine.runtimes] section
+    echo
+    if ! grep -q "^ *krun =" $HOME_CONTAINERS_CONF
+    then
+        sed --in-place -e "/^\[engine.runtimes\]/akrun = [\n   \"$KRUN_PATH\",\n]\n" $HOME_CONTAINERS_CONF
+    else
+        echo "runtime krun already configured in $HOME_CONTAINERS_CONF"
+        grep -A 3 "^ *krun =" $HOME_CONTAINERS_CONF
+    fi
 fi
 
 # If the system has selinux enabled setup our policy
 if selinux_is_enabled
 then
-   echo "selinux is present on this system"
-
-   # set selinux context on /opt/kontain/bin/km
-   if [ -e "$KM_PATH" ]; then
-      chcon $KM_SELINUX_CONTEXT $KM_PATH
-   else
-      echo "$KM_PATH does not exist, when it does exist run the following:"
-      echo "chcon $KM_SELINUX_CONTEXT $KM_PATH"
-   fi
-
-   # Add kontain selinux policy adjustments
-   mkdir -p $POLDIR
-   pushd $POLDIR || exit
+    echo "selinux is present on this system"
+    
+    # set selinux context on /opt/kontain/bin/km
+    if [ -e "$KM_PATH" ]; then
+        chcon $KM_SELINUX_CONTEXT $KM_PATH
+    else
+        echo "$KM_PATH does not exist, when it does exist run the following:"
+        echo "chcon $KM_SELINUX_CONTEXT $KM_PATH"
+    fi
+    
+    # Add kontain selinux policy adjustments
+    mkdir -p $POLDIR
+    pushd $POLDIR || exit
    cat <<EOF >$KONTAIN_SELINUX_POLICY.te
 module $KONTAIN_SELINUX_POLICY 1.0.0;
 
@@ -256,17 +256,17 @@ allow container_t kvm_device_t:chr_file { append getattr ioctl lock open read wr
 # This is a known issue, see:
 # https://bugzilla.redhat.com/show_bug.cgi?id=1861968
 EOF
-
+    
    cat <<EOF >$KONTAIN_SELINUX_POLICY.fc
 $KKM_DEVICE		-c	gen_context(system_u:object_r:kvm_device_t,s0)
 $KM_PATH		--	gen_context(system_u:object_r:bin_t,s0)
 EOF
-
-   ln -sf /usr/share/selinux/devel/Makefile
-   make
-   make reload
-   restorecon -i -F $KKM_DEVICE $KM_PATH
-   popd || exit
+    
+    ln -sf /usr/share/selinux/devel/Makefile
+    make
+    make reload
+    restorecon -i -F $KKM_DEVICE $KM_PATH
+    popd || exit
 else
-   echo "selinux not enabled on this system"
+    echo "selinux not enabled on this system"
 fi
