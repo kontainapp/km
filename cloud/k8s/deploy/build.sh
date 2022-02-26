0ed848f7db88108d635cc3a72c0dfbd88e1d1af1
Sanjay Mijar <sanjay@kontain.app>
2022-02-25 17:17:46 -0800

    added k3s related overlay and fix for containerd

--- cloud/k8s/deploy/build.sh
+++ cloud/k8s/deploy/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -x
+mkdir -p ./kontain-deploy/outputs/
 kustomize build kontain-deploy/base > ./kontain-deploy/outputs/base.yaml
 kustomize build kontain-deploy/overlays/kkm  > ./kontain-deploy/outputs/kkm.yaml
 kustomize build kontain-deploy/overlays/km-crio  > ./kontain-deploy/outputs/km-crio.yaml
+kustomize build kontain-deploy/overlays/k3s  > ./kontain-deploy/outputs/k3s.yaml
