5ca0c5a179e33e25937cebe3652e1df7e932d200
smijar <1569610+smijar@users.noreply.github.com>
2022-05-26 13:51:38 -0500

    #1590 - using current_release.txt to read release artifact URL programmatically (#1592)
    * #1590 - using current_release.txt to read release artifact URL programatically
    
    * changed filename to reflect km yaml
    
    * added relevant comment in local kustomize file used for localized testing
    
    * ignoring logs that get generated in .vscode folder

--- cloud/k8s/deploy/build_overlays.sh
+++ cloud/k8s/deploy/build_overlays.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -x
+
+# this file is only used for localized testing of kustomize outputs during edits to daemonset install
 mkdir -p ./kontain-deploy/outputs/
-kustomize build kontain-deploy/base > ./kontain-deploy/outputs/base.yaml
+kustomize build kontain-deploy/base > ./kontain-deploy/outputs/km.yaml
 kustomize build kontain-deploy/overlays/kkm  > ./kontain-deploy/outputs/kkm.yaml
 kustomize build kontain-deploy/overlays/km-crio  > ./kontain-deploy/outputs/km-crio.yaml
 kustomize build kontain-deploy/overlays/k3s  > ./kontain-deploy/outputs/k3s.yaml
