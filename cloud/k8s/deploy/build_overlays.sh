8fe03f66fc5189096ee653c5f7fa1ff4db71a843
Sanjay Mijar <sanjay@kontain.app>
2022-06-02 01:58:22 +0000

    removed comments as it might mess up kustomize

--- cloud/k8s/deploy/build_overlays.sh
+++ cloud/k8s/deploy/build_overlays.sh
@@ -6,4 +6,3 @@ mkdir -p ./kontain-deploy/outputs/
 kustomize build kontain-deploy/base > ./kontain-deploy/outputs/km.yaml
 kustomize build kontain-deploy/overlays/kkm  > ./kontain-deploy/outputs/kkm.yaml
 kustomize build kontain-deploy/overlays/km-crio  > ./kontain-deploy/outputs/km-crio.yaml
-kustomize build kontain-deploy/overlays/k3s  > ./kontain-deploy/outputs/k3s.yaml
