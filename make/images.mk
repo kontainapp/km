4e5b52cd84aef1db689a02b2b8cd7d58b8f5641c
Elana Zobin <leka@gnode.org>
2022-06-22 10:55:18 -0700

    checkpoint

--- make/images.mk
+++ make/images.mk
@@ -305,10 +305,10 @@ test-all-withdocker: ## a special helper to run more node.km tests.
 	${DOCKER_RUN_TEST} ${TEST_IMG_TAGGED} ${CONTAINER_TEST_ALL_CMD}
 
 test-coverage-withdocker: ## Run tests in local Docker. IMAGE_VERSION (i.e. tag) needs to be passed in
-	${DOCKER_RUN_TEST} --name covcontainer_${IMAGE_VERSION} ${COVERAGE_TEST_IMG_TAGGED} \
-		sh -c "${CONTAINER_TEST_CMD} && ${DOCKER_COVERAGE_CMD}"
 	echo "report FRON: ${DOCKER_COVERAGE_KM_BLDDIR}/report.json"
 	echo "report TO: ${COVERAGE_KM_BLDDIR}"
+	${DOCKER_RUN_TEST} --name covcontainer_${IMAGE_VERSION} ${COVERAGE_TEST_IMG_TAGGED} \
+		sh -c "${CONTAINER_TEST_CMD} && ${DOCKER_COVERAGE_CMD}"
 	docker cp `docker ps -aq -f name=covcontainer_${IMAGE_VERSION}`:${DOCKER_COVERAGE_KM_BLDDIR}/report.json ${COVERAGE_KM_BLDDIR}
 
 # === BUILDENV LOCAL
