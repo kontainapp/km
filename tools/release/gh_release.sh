59752c05c81380ab1361bc66f362aba6bbdb0a9e
Elana Zobin <leka@gnode.org>
2023-09-04 11:11:32 -0700

    make release using github cli

---
+++ tools/release/gh_release.sh
@@ -0,0 +1,66 @@
+#!/bin/bash 
+
+[ "$TRACE" ] && set -x
+
+# sudo dnf install 'dnf-command(config-manager)'
+# sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+# sudo dnf install gh -y
+
+# Use this is the version name is not compliant with expectations
+RELEASE_DEFAULT_VERSION="v0.1-test"
+# Delete these if they already exist
+OVERRIDABLE_RELEASES=($RELEASE_DEFAULT_VERSION, "v0.1-beta", "v0.1-edge")
+
+files=
+
+# by default use current tag 
+for arg in "$@"
+do
+   case "$arg" in
+        --token=*)
+            gh_token="${1#*=}"
+            shift
+        ;;
+        --tag=*)
+            tag="${1#*=}"
+            shift
+        ;;
+        --message=*)
+            message="${1#*=}"
+            shift
+        ;;
+        --*)
+            echo Unknown flag "${1}"
+            exit 1
+        ;;
+        *)
+            files="$files ${1}"
+            shift
+        ;;
+ esac
+done
+
+if [ -z "$tag" ]; then 
+    tag=$RELEASE_DEFAULT_VERSION
+fi
+
+if [[ $tag != v* ]]; then 
+    echo Triggered by a non-standard version. Using default test version $RELEASE_DEFAULT_VERSION
+    tag=$RELEASE_DEFAULT_VERSION
+fi   
+
+echo $gh_token | gh auth login --with-token
+gh config set prompt disabled
+
+# Github releases require an unique name. If the version is in overridables,
+# we will delete the release, no questions asked.
+# Otherwise, we will error out.
+lookup=$(gh release view $tag --json tagName| jq -r ".tagName")
+if [ -n "$lookup" ]; then 
+    if [[ ${OVERRIDABLE_RELEASES[@]} =~ $tag ]]; then 
+        gh release delete $tag -y
+    fi
+fi
+
+
+gh release create "$tag" --draft --prerelease --title "Kontain $tag" --notes "$message" $files
